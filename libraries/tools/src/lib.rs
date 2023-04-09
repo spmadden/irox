3c5a6f9825aba1516ada921e1bbeb9b9615d374e
Sean Madden <sean@seanmadden.net>
2023-04-09 17:25:50 -0400

    feat(tools): New Read utilities module

--- libraries/tools/src/lib.rs
+++ libraries/tools/src/lib.rs
@@ -4,5 +4,6 @@ pub mod bits;
 pub mod f64;
 pub mod iterators;
 pub mod packetio;
+pub mod read;
 pub mod u64;
 pub mod u8;
---
+++ libraries/tools/src/read.rs
@@ -0,0 +1,49 @@
+use std::collections::VecDeque;
+use std::io::{Error, Read};
+
+///
+/// Consumes data from the input stream until:
+/// 1. The byte stream represented by 'search' has been found or
+/// 2. The input reader returns 0 bytes read (or errors out)
+///
+/// Note: The input stream position is left JUST AFTER the found search string.
+pub fn read_until<T: Read>(input: &mut T, search: &[u8]) -> Result<(), Error> {
+    let mut ringbuf: VecDeque<u8> = VecDeque::with_capacity(search.len());
+    input.read_exact(ringbuf.as_mut_slices().0)?;
+
+    let mut onebuf: [u8; 1] = [0; 1];
+    loop {
+        if ringbuf.eq(&search) {
+            return Ok(());
+        }
+
+        if input.read(&mut onebuf)? == 0 {
+            return Ok(());
+        }
+
+        ringbuf.pop_front();
+        ringbuf.push_back(onebuf[0]);
+    }
+}
+
+///
+/// Reads the exact amount of bytes into an array and returns it
+/// ```
+/// # use irox_tools::read::read_exact;
+/// # let mut input : Vec<u8> = Vec::new();
+///
+/// let buf : [u8;10] = read_exact(&mut input)?;
+/// ```
+pub fn read_exact<T: Read, const N: usize>(input: &mut T) -> Result<[u8; N], Error> {
+    let mut buf: [u8; N] = [0; N];
+    input.read_exact(&mut buf)?;
+    Ok(buf)
+}
+
+///
+/// Reads the exact amount of bytes specified into a vector, and returns it
+pub fn read_exact_vec<T: Read>(input: &mut T, size: usize) -> Result<Vec<u8>, Error> {
+    let mut buf: Vec<u8> = vec![0; size];
+    input.read_exact(buf.as_mut_slice())?;
+    Ok(buf)
+}
