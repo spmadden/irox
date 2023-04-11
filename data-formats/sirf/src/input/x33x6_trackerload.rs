bd904caff881504b4ffd9e19233a468dce0e3acc
Sean Madden <sean@seanmadden.net>
2023-04-10 20:13:50 -0400

    Add 0x33-6 Tracker Load Statu

---
+++ data-formats/sirf/src/input/x33x6_trackerload.rs
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+// Copyright 2023 IROX Contributors
+
+use irox_tools::bits::{Bits, MutBits};
+use irox_tools::packetio::{Packet, PacketBuilder};
+
+#[derive(Default, Debug, Copy, Clone)]
+pub struct TrackerLoadStatus {
+    load_state: u32,
+    reserved_1: u32,
+    load_error: u32,
+    time_tag: u32,
+}
+
+impl Packet for TrackerLoadStatus {
+    type PacketType = ();
+    type Error = ();
+
+    fn write_to<T: MutBits>(&self, out: &mut T) -> Result<(), Self::Error> {
+        todo!()
+    }
+
+    fn get_bytes(&self) -> Result<Vec<u8>, Self::Error> {
+        todo!()
+    }
+
+    fn get_type(&self) -> Self::PacketType {
+        todo!()
+    }
+}
+
+pub struct TrackerLoadBuilder;
+pub static BUILDER: TrackerLoadBuilder = TrackerLoadBuilder;
+impl PacketBuilder<TrackerLoadStatus> for TrackerLoadBuilder {
+    type Error = std::io::Error;
+
+    fn build_from<T: Bits>(&self, input: &mut T) -> Result<TrackerLoadStatus, Self::Error> {
+        Ok(TrackerLoadStatus {
+            load_state: input.read_be_u32()?,
+            reserved_1: input.read_be_u32()?,
+            load_error: input.read_be_u32()?,
+            time_tag: input.read_be_u32()?,
+        })
+    }
+}
