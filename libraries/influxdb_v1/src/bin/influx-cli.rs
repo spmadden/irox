28014efee2b39dc799250275cb0666b026b7d0db
Sean Madden <sean@seanmadden.net>
2023-08-16 12:38:52 -0400

    feat(influx-cli): basic ping functions

--- libraries/influxdb_v1/src/bin/influx-cli.rs
+++ libraries/influxdb_v1/src/bin/influx-cli.rs
@@ -1,4 +1,69 @@
 // SPDX-License-Identifier: MIT
 // Copyright 2023 IROX Contributors
 
-fn main() {}
+use std::process::ExitCode;
+
+use clap::{Parser, Subcommand};
+use log::{debug, error, info};
+
+use irox_influxdb_v1::{InfluxConnectionBuilder, InfluxDB, InfluxDBConnectionParams};
+
+#[derive(Subcommand, Debug)]
+enum Operation {
+    /// Ping the server to check aliveness
+    PING,
+}
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about, propagate_version = true)]
+struct Config {
+    /// Command to execute
+    #[command(subcommand)]
+    command: Operation,
+
+    /// Server (IP or Hostname) to connect to - defaults to 'localhost'
+    #[arg(short = 's', long = "server")]
+    server: Option<String>,
+
+    /// Port to connect to - defaults to '8086'
+    #[arg(short = 'p', long = "port")]
+    port: Option<u16>,
+
+    /// URL to connect to - defaults to 'http://localhost:8086'
+    #[arg(short = 'u', long = "url")]
+    url: Option<String>,
+}
+
+fn main() -> ExitCode {
+    env_logger::init();
+
+    let config = Config::parse();
+
+    let conn = match match &config.url {
+        None => InfluxConnectionBuilder::default()
+            .maybe_host(config.server.clone())
+            .maybe_port(config.port)
+            .build(),
+        Some(url) => InfluxDBConnectionParams::open_url(url),
+    } {
+        Ok(db) => db,
+        Err(e) => {
+            error!("{:?}", e);
+            return ExitCode::FAILURE;
+        }
+    };
+
+    debug!("{:?}", config);
+    match config.command {
+        Operation::PING => ping(&conn),
+    }
+}
+
+fn ping(db: &InfluxDB) -> ExitCode {
+    if let Err(e) = db.ping() {
+        error!("{:?}", e);
+        return ExitCode::FAILURE;
+    }
+    info!("PING SUCCESSFUL.");
+    ExitCode::SUCCESS
+}
--- libraries/influxdb_v1/src/lib.rs
+++ libraries/influxdb_v1/src/lib.rs
@@ -1,61 +1,118 @@
 // SPDX-License-Identifier: MIT
 // Copyright 2023 IROX Contributors
 
+use url::Url;
+
 use error::{Error, ErrorType};
 use irox_networking::http::HttpProtocol;
 
 pub mod error;
 
 #[derive(Debug, Clone, Eq, PartialEq)]
-pub struct ConnectionParams {
+pub struct InfluxDBConnectionParams {
     pub(crate) host: String,
     pub(crate) port: u16,
-    pub(crate) protocol: HttpProtocol,
+    pub(crate) scheme: HttpProtocol,
 }
 
-impl Default for ConnectionParams {
+impl Default for InfluxDBConnectionParams {
     fn default() -> Self {
-        ConnectionParams {
+        InfluxDBConnectionParams {
             host: String::from("localhost"),
             port: 8086,
-            protocol: HttpProtocol::HTTP,
+            scheme: HttpProtocol::HTTP,
         }
     }
 }
 
-impl ConnectionParams {
-    pub fn open(&self) -> Result<InfluxDB, error::Error> {
+impl InfluxDBConnectionParams {
+    pub fn open(&self) -> Result<InfluxDB, Error> {
+        let base_url_str = format!("{}://{}:{}", self.scheme.name(), self.host, self.port);
+        let base_url = Url::parse(&base_url_str)?;
+        Self::open_url(base_url)
+    }
+
+    pub fn open_url(base_url_str: impl AsRef<str>) -> Result<InfluxDB, Error> {
+        let base_url = Url::parse(base_url_str.as_ref())?;
         let agent = ureq::AgentBuilder::new()
             .max_idle_connections(100)
             .max_idle_connections_per_host(200)
             .redirect_auth_headers(ureq::RedirectAuthHeaders::SameHost)
             .no_delay(true)
             .build();
-        let base_url_str = format!("{}://{}:{}", self.protocol.name(), self.host, self.port);
-        let base_url = url::Url::parse(&base_url_str)?;
-        Ok(InfluxDB {
-            agent, base_url_str, base_url
-        })
+        Ok(InfluxDB { agent, base_url })
+    }
+}
+
+#[derive(Default)]
+pub struct InfluxConnectionBuilder {
+    host: Option<String>,
+    port: Option<u16>,
+    scheme: Option<HttpProtocol>,
+}
+
+impl InfluxConnectionBuilder {
+    pub fn with_host(mut self, host: impl Into<String>) -> Self {
+        self.host = Some(host.into());
+        self
+    }
+    pub fn maybe_host(mut self, host: Option<String>) -> Self {
+        self.host = host;
+        self
+    }
+
+    pub fn with_port(mut self, port: impl Into<u16>) -> Self {
+        self.port = Some(port.into());
+        self
+    }
+
+    pub fn maybe_port(mut self, port: Option<u16>) -> Self {
+        self.port = port;
+        self
+    }
+
+    pub fn with_scheme(mut self, scheme: HttpProtocol) -> Self {
+        self.scheme = Some(scheme);
+        self
+    }
+
+    pub fn maybe_scheme(mut self, scheme: Option<HttpProtocol>) -> Self {
+        self.scheme = scheme;
+        self
+    }
+
+    pub fn build(self) -> Result<InfluxDB, Error> {
+        let mut params = InfluxDBConnectionParams::default();
+        if let Some(host) = self.host {
+            params.host = host;
+        }
+        if let Some(port) = self.port {
+            params.port = port;
+        }
+        if let Some(scheme) = self.scheme {
+            params.scheme = scheme;
+        }
+
+        params.open()
     }
 }
 
 #[derive(Clone)]
 pub struct InfluxDB {
     agent: ureq::Agent,
-    base_url_str: String,
-    base_url: url::Url
+    base_url: Url,
 }
 
 impl InfluxDB {
-    pub fn open(params: &ConnectionParams) -> Result<InfluxDB, error::Error> {
+    pub fn open(params: &InfluxDBConnectionParams) -> Result<InfluxDB, Error> {
         params.open()
     }
 
-    pub fn open_default() -> Result<InfluxDB, error::Error> {
-        ConnectionParams::default().open()
+    pub fn open_default() -> Result<InfluxDB, Error> {
+        InfluxDBConnectionParams::default().open()
     }
 
-    pub fn ping(&self) -> Result<(), error::Error>{
+    pub fn ping(&self) -> Result<(), Error> {
         let mut url = self.base_url.clone();
         url.set_path("ping");
         let req = self.agent.request_url("GET", &url);
@@ -63,8 +120,8 @@ impl InfluxDB {
         let resp = req.call()?;
         let status = resp.status();
         match status {
-            200|204 => Ok(()),
-            _ => Error::err(ErrorType::RequestErrorCode(status), "Bad Ping Response")
+            200 | 204 => Ok(()),
+            _ => Error::err(ErrorType::RequestErrorCode(status), "Bad Ping Response"),
         }
     }
 }
