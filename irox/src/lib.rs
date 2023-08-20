0bdd1d966cfbc6b4a18a19c860bb97925d2371b9
Sean P. Madden <sean@seanmadden.net>
2023-08-20 17:58:26 -0400

    feat(irox): pull in the new modules

--- irox/src/lib.rs
+++ irox/src/lib.rs
@@ -1,12 +1,24 @@
 // SPDX-License-Identifier: MIT
 // Copyright 2023 IROX Contributors
 
+#![forbid(unsafe_code)]
+
 // libraries
 #[cfg(feature = "carto")]
 pub use irox_carto;
 // data-formats
+#[cfg(feature = "csv")]
+pub use irox_csv;
+#[cfg(feature = "egui-extras")]
+pub use irox_egui_extras;
+#[cfg(feature = "influxdb_v1")]
+pub use irox_influxdb_v1;
+#[cfg(feature = "networking")]
+pub use irox_networking;
 #[cfg(feature = "sirf")]
 pub use irox_sirf;
+#[cfg(feature = "stats")]
+pub use irox_stats;
 #[cfg(feature = "tools")]
 pub use irox_tools;
 #[cfg(feature = "units")]
