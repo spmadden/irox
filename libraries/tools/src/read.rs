0eb5f64a97b2383434548873a0038f1d96b94bfb
Sean Madden <sean@seanmadden.net>
2023-09-03 20:59:36 -0400

    feat(tools): new read_until, read_exact_into, and read_exact_into_sized functions

--- libraries/tools/src/read.rs
+++ libraries/tools/src/read.rs
@@ -2,7 +2,7 @@
 // Copyright 2023 IROX Contributors
 
 use std::collections::VecDeque;
-use std::io::{Error, Read};
+use std::io::{Error, Read, Write};
 
 ///
 /// Consumes data from the input stream until:
@@ -10,13 +10,13 @@ use std::io::{Error, Read};
 /// 2. The input reader returns 0 bytes read (or errors out)
 ///
 /// Note: The input stream position is left JUST AFTER the found search string.
-pub fn read_until<T: Read>(input: &mut T, search: &[u8]) -> Result<(), Error> {
+pub fn consume_until<T: Read>(input: &mut T, search: &[u8]) -> Result<(), Error> {
     let mut ringbuf: VecDeque<u8> = VecDeque::with_capacity(search.len());
     input.read_exact(ringbuf.as_mut_slices().0)?;
 
     let mut onebuf: [u8; 1] = [0; 1];
     loop {
-        if ringbuf.eq(&search) {
+        if ringbuf.iter().eq(search) {
             return Ok(());
         }
 
@@ -25,6 +25,33 @@ pub fn read_until<T: Read>(input: &mut T, search: &[u8]) -> Result<(), Error> {
         }
 
         ringbuf.pop_front();
+        ringbuf.push_back(onebuf[0]);
+    }
+}
+
+///
+/// Reads from the input stream until:
+/// 1. The byte stream represented by 'search' has been found or
+/// 2. The input stream returns 0 bytes read (or errors out)
+/// It returns all bytes read in the interim
+pub fn read_until<T: Read>(mut input: &mut T, search: &[u8]) -> Result<Vec<u8>, Error> {
+    let mut ringbuf: VecDeque<u8> = VecDeque::with_capacity(search.len());
+
+    let mut out = Vec::new();
+    let mut onebuf: [u8; 1] = [0; 1];
+    loop {
+        if ringbuf.iter().eq(search) {
+            return Ok(out);
+        }
+
+        if input.read(&mut onebuf)? == 0 {
+            return Ok(out);
+        }
+        out.push(onebuf[0]);
+
+        if ringbuf.len() == search.len() {
+            ringbuf.pop_front();
+        }
         ringbuf.push_back(onebuf[0]);
     }
 }
@@ -50,4 +77,27 @@ pub fn read_exact_vec<T: Read>(input: &mut T, size: usize) -> Result<Vec<u8>, Er
     let mut buf: Vec<u8> = vec![0; size];
     input.read_exact(buf.as_mut_slice())?;
     Ok(buf)
+}
+
+///
+/// Reads the exact amount of bytes specified and writes it into the target output
+pub fn read_exact_into<R: Read, W: Write, const N: usize>(
+    input: &mut R,
+    output: &mut W,
+) -> Result<(), Error> {
+    let mut buf: [u8; N] = [0; N];
+    input.read_exact(&mut buf)?;
+    output.write_all(&buf)
+}
+
+///
+/// Reads the exact amount of bytes specified and writes it into the target output
+pub fn read_exact_into_sized<R: Read, W: Write>(
+    input: &mut R,
+    output: &mut W,
+    size: usize,
+) -> Result<(), Error> {
+    let mut buf: Vec<u8> = vec![0; size];
+    input.read_exact(&mut buf)?;
+    output.write_all(&buf)
 }
