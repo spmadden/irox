c63cb79f178337401d6a19111eef83229188b45d
Sean Madden <sean@seanmadden.net>
2023-09-04 16:23:45 -0400

    feat(tools): new collect_exact, collect_exact_or, collect_exact_or_default methods in itertools

--- libraries/tools/src/iterators/mod.rs
+++ libraries/tools/src/iterators/mod.rs
@@ -19,6 +19,53 @@ pub trait Itertools: Iterator {
             items: self.collect(),
         }
     }
+
+    ///
+    /// Collects a specific amount of items from the iterator.  The underlying iterator may return
+    /// more, or less items than the specified count.  Any remaining uncollected items will be
+    /// discarded.  If count is greater than the length of the underlying iterator, then the
+    /// remainder of the returned vector will be None.
+    fn collect_exact(mut self, count: usize) -> Vec<Option<Self::Item>>
+    where
+        Self: Sized + Iterator,
+        Self::Item: Clone,
+    {
+        let mut out: Vec<Option<Self::Item>> = Vec::with_capacity(count);
+        for _i in 0..count {
+            out.push(self.next())
+        }
+        out
+    }
+
+    ///
+    /// Same behavior as ['collect_exact'], except rather than returning 'None' after the end, it
+    /// returns the specified value
+    fn collect_exact_or(mut self, count: usize, def: Self::Item) -> Vec<Self::Item>
+    where
+        Self: Sized + Iterator,
+        Self::Item: Clone,
+    {
+        let mut out: Vec<Self::Item> = Vec::with_capacity(count);
+        for _i in 0..count {
+            out.push(self.next().unwrap_or(def.clone()))
+        }
+        out
+    }
+
+    ///
+    /// Same behavior as ['collect_exact'], except rather than returning 'None' after the end, it
+    /// returns the default value
+    fn collect_exact_or_default(mut self, count: usize) -> Vec<Self::Item>
+    where
+        Self: Sized + Iterator,
+        Self::Item: Clone + Default,
+    {
+        let mut out: Vec<Self::Item> = Vec::with_capacity(count);
+        for _i in 0..count {
+            out.push(self.next().unwrap_or(Default::default()))
+        }
+        out
+    }
 }
 
 impl<T: ?Sized> Itertools for T where T: Iterator {}
