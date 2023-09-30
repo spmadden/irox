d280045a9c918c9d94b77b9b812b1c43a9d918bd
Sean P. Madden <sean@seanmadden.net>
2023-09-30 09:30:26 -0400

    feat(tools): UpperHex for PrettyVec and new PrettyVecDeque

--- libraries/tools/src/vec.rs
+++ libraries/tools/src/vec.rs
@@ -1,11 +1,14 @@
 // SPDX-License-Identifier: MIT
 // Copyright 2023 IROX Contributors
 
-use std::fmt::{Display, Formatter};
+use std::collections::VecDeque;
+use std::fmt::{Display, format, Formatter, UpperHex, Write};
 
 ///
 /// This struct purely exists to implement 'Display' for a borrowed vec, whose elements implement [`std::fmt::Display`]
 pub struct PrettyVec<'a, T>(pub &'a Vec<T>);
+
+pub struct PrettyVecDeque<'a, T>(pub &'a VecDeque<T>);
 
 impl<'a, T> Display for PrettyVec<'a, T>
 where
@@ -18,5 +21,25 @@ where
         } else {
             f.write_fmt(format_args!("{vals:?}"))
         }
+    }
+}
+
+impl<'a, T> UpperHex for PrettyVec<'a, T> where T: UpperHex {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let mut buf = String::new();
+        for val in self.0 {
+            buf.write_fmt(format_args!("{val:0X}"))?;
+        }
+        f.write_fmt(format_args!("{buf}"))
+    }
+}
+
+impl<'a, T> UpperHex for PrettyVecDeque<'a, T> where T: UpperHex {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let mut buf = String::new();
+        for val in self.0 {
+            buf.write_fmt(format_args!("{val:0X}"))?;
+        }
+        f.write_fmt(format_args!("{buf}"))
     }
 }
