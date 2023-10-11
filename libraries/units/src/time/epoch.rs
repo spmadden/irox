ad582e4fb0e713c2fdd1ad5eb183fe32033e884e
Sean Madden <sean@seanmadden.net>
2023-10-10 23:29:23 -0400

    feat(units): Refactor Epochs to be like CompassHeading

--- libraries/units/src/time/epoch.rs
+++ libraries/units/src/time/epoch.rs
@@ -1,97 +1,135 @@
 // SPDX-License-Identifier: MIT
 // Copyright 2023 IROX Contributors
 
+use std::marker::PhantomData;
+
 use crate::time::{Date, Duration};
 
 ///
 /// The "epoch" serves as a reference point from which time is measured.
-pub struct Epoch {
-    date: Date,
-}
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd)]
+pub struct Epoch(pub Date);
 impl Epoch {
     ///
     /// The Gregorian Date of this particular Epoch.
     pub fn get_gregorian_date(&self) -> Date {
-        self.date
+        self.0
     }
 }
 
 ///
 /// Represents a duration offset from a particular [`Epoch`]
-pub trait Timestamp {
-    ///
-    /// Returns the Epoch associated with this timestamp.
-    fn get_epoch(&self) -> Epoch;
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct Timestamp<T> {
+    epoch: Epoch,
+    offset: Duration,
 
-    /// Returns the duration offset of the epoch associated with this timestamp.
-    fn get_duration(&self) -> Duration;
+    _phantom: PhantomData<T>,
+}
+impl<T> Timestamp<T> {
+    ///
+    /// Returns the base epoch for this timestamp
+    #[must_use]
+    pub fn get_epoch(&self) -> Epoch {
+        self.epoch
+    }
+
+    ///
+    /// Returns the relative offset of this timestamp from the specified epoch.
+    #[must_use]
+    pub fn get_offset(&self) -> Duration {
+        self.offset
+    }
 }
 
 ///
 /// The Unix Epoch, 1970-01-01, 00:00:00
-pub const UNIX_EPOCH: Epoch = Epoch {
-    date: Date {
-        year: 1970,
-        day_of_year: 1,
-    },
-};
+pub const UNIX_EPOCH: Epoch = Epoch(Date {
+    year: 1970,
+    day_of_year: 1,
+});
 
 ///
 /// Represents a duration offset from the [`UNIX_EPOCH`].
-pub struct UnixTimestamp(Duration);
-impl Timestamp for UnixTimestamp {
-    fn get_epoch(&self) -> Epoch {
-        UNIX_EPOCH
-    }
+pub type UnixTimestamp = Timestamp<UnixEpoch>;
+pub struct UnixEpoch;
 
-    fn get_duration(&self) -> Duration {
-        self.0
+macro_rules! derive_timestamp_impl {
+    ($epoch:ident,$name:ident) => {
+        impl $name {
+            ///
+            /// Creates a new timestamp given the specified offset
+            pub fn from_offset(offset: Duration) -> $name {
+                $name {
+                    epoch: $epoch,
+                    offset,
+                    ..$name::default()
+                }
+            }
+
+            ///
+            /// Creates a new timestamp given the specified number of seconds
+            pub fn from_seconds(seconds: u32) -> $name {
+                $name::from_offset(Duration::new_seconds(seconds as f64))
+            }
+        }
+        impl Default for $name {
+            fn default() -> Self {
+                $name {
+                    epoch: $epoch,
+                    offset: Duration::default(),
+
+                    _phantom: Default::default(),
+                }
+            }
+        }
+        impl From<Duration> for $name {
+            fn from(value: Duration) -> Self {
+                $name::from_offset(value)
+            }
+        }
+    };
+}
+
+impl UnixTimestamp {
+    ///
+    /// Returns the local system clock equivalent of the unix timestamp
+    pub fn now() -> UnixTimestamp {
+        match std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH) {
+            Ok(t) => UnixTimestamp::from_offset(t.into()),
+            Err(t) => {
+                UnixTimestamp::from_offset(Duration::new_seconds(-1.0 * t.duration().as_secs_f64()))
+            }
+        }
     }
 }
+derive_timestamp_impl!(UNIX_EPOCH, UnixTimestamp);
 
 ///
 /// The GPS Epoch, 1980-01-06, 00:00:00
-pub const GPS_EPOCH: Epoch = Epoch {
-    date: Date {
-        year: 1980,
-        day_of_year: 6,
-    },
-};
+pub const GPS_EPOCH: Epoch = Epoch(Date {
+    year: 1980,
+    day_of_year: 6,
+});
 
 ///
 /// Represents a duration offset from the [`GPS_EPOCH`]
-pub struct GPSTimestamp(Duration);
-impl Timestamp for GPSTimestamp {
-    fn get_epoch(&self) -> Epoch {
-        GPS_EPOCH
-    }
-
-    fn get_duration(&self) -> Duration {
-        self.0
-    }
-}
+pub type GPSTimestamp = Timestamp<GPSEpoch>;
+pub struct GPSEpoch;
+derive_timestamp_impl!(GPS_EPOCH, GPSTimestamp);
 
 ///
 /// The Gregorian Epoch, 15-OCT-1582
-pub const GREGORIAN_EPOCH: Epoch = Epoch {
-    date: Date {
-        year: 1582,
-        day_of_year: 288,
-    },
-};
+pub const GREGORIAN_EPOCH: Epoch = Epoch(Date {
+    year: 1582,
+    day_of_year: 288,
+});
 
 ///
 /// Represents a duration offset from the [`GREGORIAN_EPOCH`]
-pub struct GregorianTimestamp(Duration);
-impl Timestamp for GregorianTimestamp {
-    fn get_epoch(&self) -> Epoch {
-        GREGORIAN_EPOCH
-    }
-
-    fn get_duration(&self) -> Duration {
-        self.0
-    }
-}
+pub type GregorianTimestamp = Timestamp<GregorianEpoch>;
+pub struct GregorianEpoch;
+derive_timestamp_impl!(GREGORIAN_EPOCH, GregorianTimestamp);
 
 ///
 /// The Windows NT Epoch, 01-JAN-1601.
@@ -100,25 +138,16 @@ impl Timestamp for GregorianTimestamp {
 /// 1601 is the first year of the cycle that was active at the time Windows NT
 /// was being designed. In other words, it was chosen to make the math come out
 /// nicely.
-pub const WINDOWS_NT_EPOCH: Epoch = Epoch {
-    date: Date {
-        year: 1601,
-        day_of_year: 1,
-    },
-};
+pub const WINDOWS_NT_EPOCH: Epoch = Epoch(Date {
+    year: 1601,
+    day_of_year: 1,
+});
 
 ///
 /// Represents a duration offset from the [`WINDOWS_NT_EPOCH`]
 ///
 /// Note: when a duration is actually retrieved from the windows FILETIME
 /// routines, it comes back in 100-nanosecond increments from this epoch.
-pub struct WindowsNTTimestamp(Duration);
-impl Timestamp for WindowsNTTimestamp {
-    fn get_epoch(&self) -> Epoch {
-        WINDOWS_NT_EPOCH
-    }
-
-    fn get_duration(&self) -> Duration {
-        self.0
-    }
-}
+pub type WindowsNTTimestamp = Timestamp<WindowsEpoch>;
+pub struct WindowsEpoch;
+derive_timestamp_impl!(WINDOWS_NT_EPOCH, WindowsNTTimestamp);
