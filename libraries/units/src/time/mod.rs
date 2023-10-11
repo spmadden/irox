6d58744849e01f5b3d3c31d65a6a0d681678aff2
Sean Madden <sean@seanmadden.net>
2023-10-10 23:32:51 -0400

    fix(units): Impl extraction and display for Time

--- libraries/units/src/time/mod.rs
+++ libraries/units/src/time/mod.rs
@@ -1,12 +1,16 @@
 // SPDX-License-Identifier: MIT
 // Copyright 2023 IROX Contributors
 
+use std::fmt::{Display, Formatter};
+
+pub use datetime::*;
 pub use duration::*;
 pub use epoch::*;
 pub use gregorian::*;
 
 use crate::bounds::{GreaterThanEqualToValueError, LessThanValue, Range};
 
+mod datetime;
 mod duration;
 mod epoch;
 mod gregorian;
@@ -22,12 +26,64 @@ impl Time {
         LessThanValue::new(86401).check_value_is_valid(&second_of_day)?;
         Ok(Time { second_of_day })
     }
+
+    ///
+    /// Returns the number of seconds into the "current day"
+    #[must_use]
+    pub fn as_seconds(&self) -> u32 {
+        self.second_of_day
+    }
+
+    ///
+    /// Converts this time into a duration
+    #[must_use]
+    pub fn as_duration(&self) -> Duration {
+        Duration::new(self.second_of_day as f64, DurationUnit::Second)
+    }
+
+    ///
+    /// Returns the number of hours represented by this time.
+    #[must_use]
+    pub fn as_hours(&self) -> u32 {
+        (self.second_of_day as f64 / SECONDS_IN_HOUR as f64) as u32
+    }
+
+    ///
+    /// Returns the number minutes
+    #[must_use]
+    pub fn as_minutes(&self) -> u32 {
+        (self.second_of_day as f64 / SECONDS_IN_MINUTE as f64) as u32
+    }
+
+    ///
+    /// Returns a triplet, (hours, minutes, seconds) representing this time
+    #[must_use]
+    pub fn as_hms(&self) -> (u32, u32, u32) {
+        let hours = self.as_hours();
+        let minutes = self.as_minutes() - hours * MINUTES_IN_HOUR;
+        let seconds = self.as_seconds() - hours * SECONDS_IN_HOUR - minutes * SECONDS_IN_MINUTE;
+        (hours, minutes, seconds)
+    }
 }
 
-pub const HOURS_IN_DAY: u8 = 24;
-pub const MINUTES_IN_HOUR: u8 = 60;
-pub const SECONDS_IN_MINUTE: u8 = 60;
-pub const MINUTES_IN_DAY: u16 = 1440;
+impl Display for Time {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let (h, m, s) = self.as_hms();
+        f.write_fmt(format_args!("{h:0}:{m:02}:{s:02}"))
+    }
+}
+
+impl From<Time> for Duration {
+    fn from(value: Time) -> Self {
+        value.as_duration()
+    }
+}
+
+pub const HOURS_IN_DAY: u32 = 24;
+pub const MINUTES_IN_HOUR: u32 = 60;
+pub const SECONDS_IN_MINUTE: u32 = 60;
+pub const MINUTES_IN_DAY: u32 = 1440;
+pub const SECONDS_IN_HOUR: u32 = 3600;
 
 ///
 /// Generally 86400, but occasionally 86401 for leap seconds.
