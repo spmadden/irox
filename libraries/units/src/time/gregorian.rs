04979ee7e59954e4f3b6ec9706354e6544205fb7
Sean Madden <sean@seanmadden.net>
2023-10-14 13:32:37 -0400

    feat(units): overhaul Gregorian

--- libraries/units/src/time/gregorian.rs
+++ libraries/units/src/time/gregorian.rs
@@ -3,7 +3,25 @@
 
 use irox_enums::{EnumIterItem, EnumName, EnumTryFromStr};
 
-use crate::bounds::{OutsideRangeError, Range, WithinRange};
+use crate::bounds::{GreaterThanEqualToValueError, LessThanValue, Range};
+use crate::time::epoch::{UnixTimestamp, UNIX_EPOCH};
+use crate::time::SECONDS_IN_DAY;
+
+/// Days per 4 Year Window
+///
+/// Each window has 1 extra leap day in it
+pub const DAYS_PER_4YEAR: u32 = 365 * 4 + 1;
+
+/// Days per 100 Year Window
+///
+/// There are 25x 4Y Windows in a 100Y Window, except the 100th isn't a Leap.
+pub const DAYS_PER_100YEAR: u32 = DAYS_PER_4YEAR * 25 - 1;
+
+///
+/// Days per 400 Year Window
+///
+/// There are 4x 100Y Windows in a 400Y Window, with an extra leap day
+pub const DAYS_PER_400YEAR: u32 = DAYS_PER_100YEAR * 4 + 1;
 
 ///
 /// Gregorian Month enumeration
@@ -11,18 +29,18 @@ use crate::bounds::{OutsideRangeError, Range, WithinRange};
     Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd, EnumName, EnumIterItem, EnumTryFromStr,
 )]
 pub enum Month {
-    January,
-    February,
-    March,
-    April,
-    May,
-    June,
-    July,
-    August,
-    September,
-    October,
-    November,
-    December,
+    January = 1,
+    February = 2,
+    March = 3,
+    April = 4,
+    May = 5,
+    June = 6,
+    July = 7,
+    August = 8,
+    September = 9,
+    October = 10,
+    November = 11,
+    December = 12,
 }
 
 impl Month {
@@ -130,17 +148,32 @@ impl Month {
     ///
     /// Returns a range verifier to check if the indicate day number is valid for
     /// the particular month and year
-    pub const fn valid_day_number(&self, year: i32) -> WithinRange<u8> {
+    #[must_use]
+    pub const fn valid_day_number(&self, year: i32) -> LessThanValue<u8> {
         let upper_lim = self.days_in_month(year);
-        WithinRange::new(0, upper_lim + 1)
+        LessThanValue::new(upper_lim + 1)
+    }
+
+    ///
+    /// Returns true if the indicate day of the year is within this month
+    #[must_use]
+    pub const fn day_is_within_month(&self, year: i32, day_of_year: u16) -> bool {
+        day_of_year >= self.start_day_of_year(year) && day_of_year <= self.end_day_of_year(year)
+    }
+
+    ///
+    /// Returns true if the indicated date is within this month
+    #[must_use]
+    pub const fn date_is_within_month(&self, date: &Date) -> bool {
+        self.day_is_within_month(date.year, date.day_of_year)
     }
 }
 
 impl TryFrom<u8> for Month {
-    type Error = OutsideRangeError<u8>;
+    type Error = GreaterThanEqualToValueError<u8>;
 
     fn try_from(value: u8) -> Result<Self, Self::Error> {
-        let range = WithinRange::new(0, 13);
+        let range = LessThanValue::new(13);
         Ok(match value {
             1 => Month::January,
             2 => Month::February,
@@ -163,29 +196,53 @@ impl TryFrom<u8> for Month {
 /// Gregorian Date - a specific date on a calendar.
 #[derive(Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd)]
 pub struct Date {
+    ///
+    /// Year is the Proleptic Gregorian Year
     pub(crate) year: i32,
+
+    ///
+    /// Day of Year is the day index into the specified year, range [0, 366)
     pub(crate) day_of_year: u16,
 }
 
 impl Date {
     ///
     /// Constructs a new date given the provided gregorian year and day of year.
-    /// Returns `Err(OutsideRangeErr)` if `day_of_year` is outside the valid range.
-    pub fn new(year: i32, day_of_year: u16) -> Result<Date, OutsideRangeError<u16>> {
+    /// Returns `Err(GreaterThanEqualToValueError)` if `day_of_year` is outside the valid range.
+    pub fn new(year: i32, day_of_year: u16) -> Result<Date, GreaterThanEqualToValueError<u16>> {
         let valid_num_days = if is_leap_year(year) { 366 } else { 365 };
 
-        WithinRange::new(0, valid_num_days + 1).check_value_is_valid(&day_of_year)?;
+        LessThanValue::new(valid_num_days).check_value_is_valid(&day_of_year)?;
 
         Ok(Date { year, day_of_year })
     }
 
     ///
     /// Constructs a new date given the provided values.  If month or day is out
-    /// of range, will return `Err(OutsideRangeError)`
-    pub fn try_from_values(year: i32, month: u8, day: u8) -> Result<Date, OutsideRangeError<u8>> {
+    /// of range, will return `Err(OutsideRangeError)`.
+    ///
+    /// Note: The 'day' parameter here is in the range of `1..=31` as would be shown on a calendar
+    pub fn try_from_values(
+        year: i32,
+        month: u8,
+        day: u8,
+    ) -> Result<Date, GreaterThanEqualToValueError<u8>> {
         let month: Month = month.try_into()?;
+        Self::try_from(year, month, day)
+    }
+
+    ///
+    /// Constructs a new date given the provided values, if day is out of range,
+    /// will return `Err(OutsideRangeError)`
+    ///
+    /// Note: The 'day' parameter here is in the range of `1..=31` as would be shown on a calendar
+    pub fn try_from(
+        year: i32,
+        month: Month,
+        day: u8,
+    ) -> Result<Date, GreaterThanEqualToValueError<u8>> {
         month.valid_day_number(year).check_value_is_valid(&day)?;
-        let day_of_year = month.start_day_of_year(year) + day as u16;
+        let day_of_year = month.start_day_of_year(year) + day as u16 - 1;
         Ok(Date { year, day_of_year })
     }
 
@@ -197,11 +254,40 @@ impl Date {
     }
 
     ///
-    /// Returns the day of the year.  January 1 is '1', January 31 is '31',
+    /// Returns the day offset into the year.  January 1 is '0', January 31 is '30',
+    /// February 1 is '31', and so on.
+    #[must_use]
+    pub fn day_of_year_offset(&self) -> u16 {
+        self.day_of_year
+    }
+
+    ///
+    /// Returns the calendar day of the year.  January 1 is '1', January 31 is '31',
     /// February 1 is '32', and so on.
     #[must_use]
     pub fn day_of_year(&self) -> u16 {
-        self.day_of_year
+        self.day_of_year + 1
+    }
+
+    ///
+    /// Returns the [`Month`] this date is contained within
+    #[must_use]
+    pub fn month_of_year(&self) -> Month {
+        for month in Month::iter_items() {
+            if month.date_is_within_month(self) {
+                return month;
+            }
+        }
+
+        // TODO: Proper normalization here.
+        Month::January
+    }
+
+    ///
+    /// Returns the day index into the current month in the range [0,31)
+    #[must_use]
+    pub fn day_of_month(&self) -> u8 {
+        (self.day_of_year - self.month_of_year().start_day_of_year(self.year)) as u8
     }
 
     /// Adds the specified number of days to this date.
@@ -209,7 +295,7 @@ impl Date {
     pub fn add_days(&self, days: u16) -> Date {
         let mut days_remaining = days;
         let mut years = self.year;
-        let mut days = self.day_of_year - 1;
+        let mut days = self.day_of_year;
 
         loop {
             let days_in_year = days_in_year(years);
@@ -224,7 +310,7 @@ impl Date {
         }
         Date {
             year: years,
-            day_of_year: days + 1,
+            day_of_year: days,
         }
     }
 
@@ -233,7 +319,7 @@ impl Date {
     pub fn sub_days(&self, days: u16) -> Date {
         let mut days_remaining = days;
         let mut years = self.year;
-        let mut days = self.day_of_year - 1;
+        let mut days = self.day_of_year;
 
         loop {
             if days_remaining > days {
@@ -248,7 +334,7 @@ impl Date {
         }
         Date {
             year: years,
-            day_of_year: days + 1,
+            day_of_year: days,
         }
     }
 
@@ -302,9 +388,100 @@ pub const fn days_in_year(year: i32) -> u16 {
     }
 }
 
+///
+/// Returns the total number of seconds in the indicated calendar year
+pub const fn seconds_in_year(year: i32) -> u32 {
+    days_in_year(year) as u32 * SECONDS_IN_DAY
+}
+
+impl From<Date> for UnixTimestamp {
+    fn from(value: Date) -> Self {
+        let years_duration = value.year - UNIX_EPOCH.0.year;
+        if years_duration < 0 {
+            return UnixTimestamp::default();
+        }
+        let mut secs_duration: u64 = value.day_of_year as u64 * SECONDS_IN_DAY as u64;
+        for year in UNIX_EPOCH.0.year..value.year {
+            secs_duration += seconds_in_year(year) as u64;
+        }
+
+        UnixTimestamp::from_seconds_f64(secs_duration as f64)
+    }
+}
+/// 01-MAR-2000, a mod400 year after the leap day.
+const LEAPOCH: UnixTimestamp = UnixTimestamp::from_seconds(951868800);
+impl From<&UnixTimestamp> for Date {
+    fn from(value: &UnixTimestamp) -> Self {
+        // Algorithm impl based on libmusl __secs_to_tm.c
+        let sec_in_day = SECONDS_IN_DAY as i64;
+        let leapoch = LEAPOCH.get_offset().as_seconds() as i64;
+        let offset = value.get_offset().as_seconds() as i64;
+
+        // clever impl - the leapoch is a nice round 400 cycle leap year
+        // so we compute the negative offset (for dates before the leapoch)
+        // and the positive offset (for dates after the leapoch).  This ensures
+        // that the "day calculation" is always 0 aligned to a round leap cycle.
+        // Therefore, there's no weird offsets and calculations and even division.
+        let seconds = offset - leapoch;
+        let mut days = seconds / sec_in_day;
+        if seconds % sec_in_day < 0 {
+            days -= 1;
+        }
+
+        // compute the number of 400 leap year (qc) cycles
+        let mut qc_cycles = days / DAYS_PER_400YEAR as i64;
+        let mut rem_days = days % DAYS_PER_400YEAR as i64;
+        if rem_days < 0 {
+            rem_days += DAYS_PER_400YEAR as i64;
+            qc_cycles -= 1;
+        }
+
+        // compute the remaining number of 100 non-leap year (c) cycles
+        let mut c_cycles = rem_days / DAYS_PER_100YEAR as i64;
+        if c_cycles == 4 {
+            c_cycles -= 1;
+        }
+        rem_days -= c_cycles * DAYS_PER_100YEAR as i64;
+
+        // compute the remaining number 4 leap year (q) cycles
+        let mut q_cycles = rem_days / DAYS_PER_4YEAR as i64;
+        if q_cycles == 25 {
+            q_cycles -= 1;
+        }
+        rem_days -= q_cycles * DAYS_PER_4YEAR as i64;
+
+        // and lastly, the number of non-leap years
+        let mut rem_years = rem_days / 365;
+        if rem_years == 4 {
+            rem_years -= 1;
+        }
+        rem_days -= rem_years * 365;
+
+        let mut year = rem_years + 4 * q_cycles + 100 * c_cycles + 400 * qc_cycles + 2000;
+        let mut yday = rem_days + 31 + 28; // because the LEAPOCH is 1-MAR
+        if is_leap_year(year as i32) {
+            if yday + 1 > 365 {
+                year += 1;
+                yday -= 366;
+            }
+            yday += 1;
+        } else if yday > 364 {
+            year += 1;
+            yday -= 365;
+        }
+
+        Date {
+            year: year as i32,
+            day_of_year: yday as u16,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::time::gregorian::is_leap_year;
+    use crate::bounds::GreaterThanEqualToValueError;
+    use crate::time::epoch::UnixTimestamp;
+    use crate::time::gregorian::{is_leap_year, Date, Month};
 
     #[test]
     pub fn leap_year_test() {
@@ -324,5 +501,27 @@ mod tests {
         assert!(!is_leap_year(1800));
         assert!(!is_leap_year(1900));
         assert!(!is_leap_year(2100));
+    }
+
+    #[test]
+    pub fn test_timestamp_to_date() -> Result<(), GreaterThanEqualToValueError<u16>> {
+        assert_eq!(
+            UnixTimestamp::from_seconds(1697299822).as_date(),
+            Date::new(2023, 286)?
+        );
+
+        assert_eq!(
+            UnixTimestamp::from_seconds(0).as_date(),
+            Date::new(1970, 0)?
+        );
+        assert_eq!(
+            UnixTimestamp::from_seconds(915148800).as_date(),
+            Date::new(1999, 0)?
+        );
+        assert_eq!(
+            UnixTimestamp::from_seconds(1095379200).as_date(),
+            Date::try_from(2004, Month::September, 17).unwrap()
+        );
+        Ok(())
     }
 }
