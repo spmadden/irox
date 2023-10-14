5d0ee4c0a813a180de0c1bd79d98d84518e509cf
Sean Madden <sean@seanmadden.net>
2023-10-14 13:30:44 -0400

    feat(tools): add collect_next_chunk method to itertools

--- libraries/tools/src/iterators/mod.rs
+++ libraries/tools/src/iterators/mod.rs
@@ -1,10 +1,17 @@
 // SPDX-License-Identifier: MIT
 // Copyright 2023 IROX Contributors
 
+//!
+//! Iterators adds the [`Itertools`] Trait, which adds a number of additional
+//! helper methods to the [`Iterator`] Trait.
+//!
+
 use self::looping_forever::LoopingForever;
 
 pub mod looping_forever;
 
+///
+/// Itertools adds helpful additional methods to [`Iterator`]
 pub trait Itertools: Iterator {
     ///
     /// Returns an iterator that never ends, sequentially looping over all items in this
@@ -14,7 +21,7 @@ pub trait Itertools: Iterator {
         Self: Sized + Iterator,
         Self::Item: Clone,
     {
-        looping_forever::LoopingForever {
+        LoopingForever {
             index: 0,
             items: self.collect(),
         }
@@ -25,6 +32,7 @@ pub trait Itertools: Iterator {
     /// more, or less items than the specified count.  Any remaining uncollected items will be
     /// discarded.  If count is greater than the length of the underlying iterator, then the
     /// remainder of the returned vector will be None.
+    #[must_use]
     fn collect_exact(mut self, count: usize) -> Vec<Option<Self::Item>>
     where
         Self: Sized + Iterator,
@@ -40,6 +48,7 @@ pub trait Itertools: Iterator {
     ///
     /// Same behavior as [`collect_exact`], except rather than returning 'None' after the end, it
     /// returns the specified value
+    #[must_use]
     fn collect_exact_or(mut self, count: usize, def: Self::Item) -> Vec<Self::Item>
     where
         Self: Sized + Iterator,
@@ -55,6 +64,7 @@ pub trait Itertools: Iterator {
     ///
     /// Same behavior as [`collect_exact`], except rather than returning 'None' after the end, it
     /// returns the default value
+    #[must_use]
     fn collect_exact_or_default(mut self, count: usize) -> Vec<Self::Item>
     where
         Self: Sized + Iterator,
@@ -63,6 +73,23 @@ pub trait Itertools: Iterator {
         let mut out: Vec<Self::Item> = Vec::with_capacity(count);
         for _i in 0..count {
             out.push(self.next().unwrap_or(Default::default()))
+        }
+        out
+    }
+
+    ///
+    /// Collects and returns upto 'count' items from this iterator.  If this
+    /// iterator is exhausted, the returned vector is empty.  Note, the returned
+    /// vector may have *up to* the requested number of items if the iterator
+    /// exhausts before filling the chunk.
+    #[must_use]
+    fn collect_next_chunk(&mut self, count: usize) -> Vec<Self::Item> {
+        let mut out: Vec<Self::Item> = Vec::with_capacity(count);
+        for _i in 0..count {
+            match self.next() {
+                Some(e) => out.push(e),
+                None => break,
+            }
         }
         out
     }
