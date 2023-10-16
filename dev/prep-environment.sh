3e535e7e5ce27736e77f08f887a66f246f026f13
Sean Madden <sean@seanmadden.net>
2023-10-15 22:50:00 -0400

    fix(build): fix typo in prep-environment.sh

--- dev/prep-environment.sh
+++ dev/prep-environment.sh
@@ -4,7 +4,7 @@ set -eux
 
 rustup update
 rustup component add rustfmt clippy
-TMPDIR=$(mktmp -d)
+TMPDIR=$(mktemp -d)
 cargo install --target-dir=$TMPDIR cargo-about cargo-deny cargo-edit cargo-smart-release just cargo-cache cargo-binstall
 cargo cache -e -k0 -rall
 rm -rf $TMPDIR
