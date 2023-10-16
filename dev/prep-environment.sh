c34805b787d68aabc94c63cdac7c366c24f236e0
Sean Madden <sean@seanmadden.net>
2023-10-16 19:51:31 -0400

    fix(build): switch to binstall

--- dev/prep-environment.sh
+++ dev/prep-environment.sh
@@ -5,6 +5,7 @@ set -eux
 rustup update
 rustup component add rustfmt clippy
 TMPDIR=$(mktemp -d)
-cargo install --target-dir=$TMPDIR cargo-about cargo-deny cargo-edit cargo-smart-release just cargo-cache cargo-binstall
+cargo install --target-dir=$TMPDIR cargo-binstall
+cargo binstall -y cargo-about cargo-deny cargo-smart-release cargo-edit just cargo-cache
 cargo cache -e -k0 -rall
 rm -rf $TMPDIR
