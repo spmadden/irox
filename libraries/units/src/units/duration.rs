8388e21e352e297da2470f048b38f254207c3032
Sean Madden <sean@seanmadden.net>
2023-10-20 10:36:36 -0400

    feat(units): add Days and Years to duration

--- libraries/units/src/units/duration.rs
+++ libraries/units/src/units/duration.rs
@@ -12,13 +12,6 @@ use crate::units::{FromUnits, Unit};
 
 ///
 /// Represents a specific duration unit - SI or otherwise.
-///
-/// This duration ONLY includes those units with fixed definitions.  Day, Month,
-/// and Year have variable durations based on context.
-///
-/// Days can have Leap Seconds,
-/// Months can have a Leap Day,
-/// Years are composed of Days and Months.
 #[derive(Debug, Clone, Copy, Default, Eq, PartialEq)]
 #[non_exhaustive]
 pub enum DurationUnit {
@@ -42,6 +35,13 @@ pub enum DurationUnit {
 
     /// An hour, 24 in a day.
     Hour,
+
+    /// NIST 811 defines a "Day" as 86400 seconds, without the concept of leap seconds.
+    Day,
+
+    /// NIST 811 defines a "Year" as 365 days, or 31_536_000 seconds, without the concept of leap
+    /// days.
+    Year,
 }
 
 impl DurationUnit {
@@ -105,6 +105,8 @@ macro_rules! from_units_duration {
                         DurationUnit::Second => value * SEC_TO_NANOS as $type,
                         DurationUnit::Minute => value * MIN_TO_NANOS as $type,
                         DurationUnit::Hour => value * HOUR_TO_NANOS as $type,
+                        DurationUnit::Day => value * DAY_TO_NANOS as $type,
+                        DurationUnit::Year => value * YEAR_TO_NANOS as $type,
                     },
                     DurationUnit::Microsecond => match units {
                         // source
@@ -114,6 +116,8 @@ macro_rules! from_units_duration {
                         DurationUnit::Second => value * SEC_TO_MILLIS as $type,
                         DurationUnit::Minute => value * MIN_TO_MICROS as $type,
                         DurationUnit::Hour => value * HOUR_TO_MICROS as $type,
+                        DurationUnit::Day => value * DAY_TO_MICROS as $type,
+                        DurationUnit::Year => value * YEAR_TO_MICROS as $type,
                     },
                     DurationUnit::Millisecond => match units {
                         // source
@@ -123,6 +127,8 @@ macro_rules! from_units_duration {
                         DurationUnit::Second => value * SEC_TO_MILLIS as $type,
                         DurationUnit::Minute => value * MIN_TO_MILLIS as $type,
                         DurationUnit::Hour => value * HOUR_TO_MILLIS as $type,
+                        DurationUnit::Day => value * DAY_TO_MILLIS as $type,
+                        DurationUnit::Year => value * YEAR_TO_MILLIS as $type,
                     },
                     DurationUnit::Second => match units {
                         // source
@@ -132,6 +138,8 @@ macro_rules! from_units_duration {
                         DurationUnit::Second => value as $type,
                         DurationUnit::Minute => value * MIN_TO_SEC as $type,
                         DurationUnit::Hour => value * HOUR_TO_SEC as $type,
+                        DurationUnit::Day => value * DAY_TO_SEC as $type,
+                        DurationUnit::Year => value * YEAR_TO_SEC as $type,
                     },
                     DurationUnit::Minute => match units {
                         // source
@@ -141,6 +149,8 @@ macro_rules! from_units_duration {
                         DurationUnit::Second => value * SEC_TO_MIN as $type,
                         DurationUnit::Minute => value as $type,
                         DurationUnit::Hour => value * HOUR_TO_MIN as $type,
+                        DurationUnit::Day => value * DAY_TO_MIN as $type,
+                        DurationUnit::Year => value * YEAR_TO_MIN as $type,
                     },
                     DurationUnit::Hour => match units {
                         // source
@@ -150,6 +160,30 @@ macro_rules! from_units_duration {
                         DurationUnit::Second => value * SEC_TO_HOUR as $type,
                         DurationUnit::Minute => value * MIN_TO_HOUR as $type,
                         DurationUnit::Hour => value as $type,
+                        DurationUnit::Day => value * DAY_TO_HOUR as $type,
+                        DurationUnit::Year => value * YEAR_TO_HOUR as $type,
+                    },
+                    DurationUnit::Day => match units {
+                        // source
+                        DurationUnit::Nanosecond => value * NANOS_TO_DAY as $type,
+                        DurationUnit::Microsecond => value * MICROS_TO_DAY as $type,
+                        DurationUnit::Millisecond => value * MILLIS_TO_DAY as $type,
+                        DurationUnit::Second => value * SEC_TO_DAY as $type,
+                        DurationUnit::Minute => value * MIN_TO_DAY as $type,
+                        DurationUnit::Hour => value * HOUR_TO_DAY as $type,
+                        DurationUnit::Day => value as $type,
+                        DurationUnit::Year => value * YEAR_TO_DAY as $type,
+                    },
+                    DurationUnit::Year => match units {
+                        // source
+                        DurationUnit::Nanosecond => value * NANOS_TO_YEAR as $type,
+                        DurationUnit::Microsecond => value * MICROS_TO_YEAR as $type,
+                        DurationUnit::Millisecond => value * MILLIS_TO_YEAR as $type,
+                        DurationUnit::Second => value * SEC_TO_YEAR as $type,
+                        DurationUnit::Minute => value * MIN_TO_YEAR as $type,
+                        DurationUnit::Hour => value * HOUR_TO_YEAR as $type,
+                        DurationUnit::Day => value * DAY_TO_YEAR as $type,
+                        DurationUnit::Year => value as $type,
                     },
                 }
             }
@@ -210,6 +244,11 @@ impl Duration {
         self.as_unit(DurationUnit::Second).value()
     }
 
+    /// Returns the value of this duration in fractional seconds
+    pub fn as_seconds_f32(&self) -> f32 {
+        self.as_unit(DurationUnit::Second).value() as f32
+    }
+
     /// Returns the value of this duration as whole milliseconds, with any
     /// fractional element truncated off.
     pub fn as_millis(&self) -> u64 {
@@ -239,6 +278,82 @@ impl Duration {
     pub fn as_hours(&self) -> u64 {
         self.as_unit(DurationUnit::Hour).value() as u64
     }
+
+    /// Returns the value of this duration as whole days, with any fractional
+    /// element truncated off
+    pub fn as_days(&self) -> u64 {
+        self.as_unit(DurationUnit::Day).value() as u64
+    }
+
+    /// Returns the value of this duration as whole years, with any fractional
+    /// element truncated off
+    pub fn as_years(&self) -> u64 {
+        self.as_unit(DurationUnit::Year).value() as u64
+    }
+}
+
+// Backwards compatibility for [`std::time::Duration`] drop-in creation
+impl Duration {
+    /// Creates a new `Duration` from the specified number of microseconds.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use irox_units::units::duration::Duration;
+    ///
+    /// let duration = Duration::from_micros(1_000_002);
+    ///
+    /// assert_eq!(1, duration.as_seconds());
+    /// ```
+    pub const fn from_micros(micros: u64) -> Duration {
+        Duration::new(micros as f64, DurationUnit::Microsecond)
+    }
+
+    /// Creates a new `Duration` from the specified number of milliseconds.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use irox_units::units::duration::Duration;
+    ///
+    /// let duration = Duration::from_millis(2569);
+    ///
+    /// assert_eq!(2, duration.as_seconds());
+    /// ```
+    pub const fn from_millis(millis: u64) -> Duration {
+        Duration::new(millis as f64, DurationUnit::Millisecond)
+    }
+
+    /// Creates a new `Duration` from the specified number of nanoseconds.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use irox_units::units::duration::Duration;
+    ///
+    /// let duration = Duration::from_nanos(1_000_000_123);
+    ///
+    /// assert_eq!(1, duration.as_seconds());
+    /// ```
+    pub const fn from_nanos(nanos: u64) -> Duration {
+        Duration::new(nanos as f64, DurationUnit::Nanosecond)
+    }
+
+    pub const fn from_minutes(minutes: u64) -> Duration {
+        Duration::new(minutes as f64, DurationUnit::Minute)
+    }
+
+    pub const fn from_hours(hours: u64) -> Duration {
+        Duration::new(hours as f64, DurationUnit::Hour)
+    }
+
+    pub const fn from_days(days: u64) -> Duration {
+        Duration::new(days as f64, DurationUnit::Day)
+    }
+
+    pub const fn from_years(years: u64) -> Duration {
+        Duration::new(years as f64, DurationUnit::Year)
+    }
 }
 
 impl Display for Duration {
@@ -253,8 +368,12 @@ pub const MICROS_TO_MILLIS: f64 = 1e-3;
 pub const MILLIS_TO_SEC: f64 = 1e-3;
 pub const SEC_TO_MIN: f64 = 1. / MIN_TO_SEC;
 pub const MIN_TO_HOUR: f64 = 1. / HOUR_TO_MIN;
+pub const HOUR_TO_DAY: f64 = 1. / DAY_TO_HOUR;
+pub const DAY_TO_YEAR: f64 = 1. / YEAR_TO_DAY;
 
 // going down
+pub const YEAR_TO_DAY: f64 = 365_f64;
+pub const DAY_TO_HOUR: f64 = 24_f64;
 pub const HOUR_TO_MIN: f64 = 60_f64;
 pub const MIN_TO_SEC: f64 = 60_f64;
 pub const SEC_TO_MILLIS: f64 = 1e3;
@@ -262,6 +381,8 @@ pub const MILLIS_TO_MICROS: f64 = 1e3;
 pub const MICROS_TO_NANOS: f64 = 1e3;
 
 // going down double jumps
+pub const YEAR_TO_HOUR: f64 = YEAR_TO_DAY * DAY_TO_HOUR;
+pub const DAY_TO_MIN: f64 = DAY_TO_HOUR * HOUR_TO_MIN;
 pub const HOUR_TO_SEC: f64 = HOUR_TO_MIN * MIN_TO_SEC;
 pub const MIN_TO_MILLIS: f64 = MIN_TO_SEC * SEC_TO_MILLIS;
 pub const SEC_TO_MICROS: f64 = SEC_TO_MILLIS * MILLIS_TO_MICROS;
@@ -272,8 +393,12 @@ pub const NANOS_TO_MILLIS: f64 = NANOS_TO_MICROS * MICROS_TO_MILLIS;
 pub const MICROS_TO_SECS: f64 = MICROS_TO_MILLIS * MILLIS_TO_SEC;
 pub const MILLIS_TO_MIN: f64 = MILLIS_TO_SEC * SEC_TO_MIN;
 pub const SEC_TO_HOUR: f64 = SEC_TO_MIN * MIN_TO_HOUR;
+pub const MIN_TO_DAY: f64 = MIN_TO_HOUR * HOUR_TO_DAY;
+pub const HOUR_TO_YEAR: f64 = HOUR_TO_DAY * DAY_TO_YEAR;
 
 // going down triples
+pub const YEAR_TO_MIN: f64 = YEAR_TO_HOUR * HOUR_TO_MIN;
+pub const DAY_TO_SEC: f64 = DAY_TO_MIN * MIN_TO_SEC;
 pub const HOUR_TO_MILLIS: f64 = HOUR_TO_SEC * SEC_TO_MILLIS;
 pub const MIN_TO_MICROS: f64 = MIN_TO_MILLIS * MILLIS_TO_MICROS;
 pub const SEC_TO_NANOS: f64 = SEC_TO_MICROS * MICROS_TO_NANOS;
@@ -282,17 +407,41 @@ pub const SEC_TO_NANOS: f64 = SEC_TO_MICROS * MICROS_TO_NANOS;
 pub const NANOS_TO_SEC: f64 = NANOS_TO_MILLIS * MILLIS_TO_SEC;
 pub const MICROS_TO_MIN: f64 = MICROS_TO_SECS * SEC_TO_MIN;
 pub const MILLIS_TO_HOUR: f64 = MILLIS_TO_MIN * MIN_TO_HOUR;
+pub const SEC_TO_DAY: f64 = SEC_TO_HOUR * HOUR_TO_DAY;
+pub const MIN_TO_YEAR: f64 = MIN_TO_DAY * DAY_TO_YEAR;
 
 // going down quads
+pub const YEAR_TO_SEC: f64 = YEAR_TO_MIN * MIN_TO_SEC;
+pub const DAY_TO_MILLIS: f64 = DAY_TO_SEC * SEC_TO_MILLIS;
 pub const HOUR_TO_MICROS: f64 = HOUR_TO_MILLIS * MILLIS_TO_MICROS;
 pub const MIN_TO_NANOS: f64 = MIN_TO_MICROS * MICROS_TO_NANOS;
 
 // going up quads
 pub const NANOS_TO_MIN: f64 = NANOS_TO_SEC * SEC_TO_MIN;
 pub const MICROS_TO_HOUR: f64 = MICROS_TO_MIN * MIN_TO_HOUR;
+pub const MILLIS_TO_DAY: f64 = MILLIS_TO_HOUR * HOUR_TO_DAY;
+pub const SEC_TO_YEAR: f64 = SEC_TO_DAY * DAY_TO_YEAR;
 
 // going down pentas
+pub const YEAR_TO_MILLIS: f64 = YEAR_TO_SEC * SEC_TO_MILLIS;
+pub const DAY_TO_MICROS: f64 = DAY_TO_MILLIS * MILLIS_TO_MICROS;
 pub const HOUR_TO_NANOS: f64 = HOUR_TO_MICROS * MICROS_TO_NANOS;
 
 // going up pentas
 pub const NANOS_TO_HOUR: f64 = NANOS_TO_MIN * MIN_TO_HOUR;
+pub const MICROS_TO_DAY: f64 = MICROS_TO_HOUR * HOUR_TO_DAY;
+pub const MILLIS_TO_YEAR: f64 = MILLIS_TO_DAY * DAY_TO_YEAR;
+
+// going down hexas
+pub const YEAR_TO_MICROS: f64 = YEAR_TO_MILLIS * MILLIS_TO_MICROS;
+pub const DAY_TO_NANOS: f64 = DAY_TO_MICROS * MICROS_TO_NANOS;
+
+// going up hexas
+pub const NANOS_TO_DAY: f64 = NANOS_TO_HOUR * HOUR_TO_DAY;
+pub const MICROS_TO_YEAR: f64 = MICROS_TO_DAY * DAY_TO_YEAR;
+
+// going down septs
+pub const YEAR_TO_NANOS: f64 = YEAR_TO_MICROS * MICROS_TO_NANOS;
+
+// going up septs
+pub const NANOS_TO_YEAR: f64 = NANOS_TO_DAY * DAY_TO_YEAR;
