92294eb04dda490695d7ab4fde72de759be4045e
Sean Madden <sean@seanmadden.net>
2023-12-05 19:33:18 -0500

    feat(egui_extras): progress bar now can draw text left, center, and right aligned

--- libraries/egui_extras/src/progressbar.rs
+++ libraries/egui_extras/src/progressbar.rs
@@ -2,22 +2,39 @@
 // Copyright ${YEAR} IROX Contributors
 //
 
+//!
+//! Augmentations and tweaks for a better progress bar
+
 use eframe::emath::lerp;
 use egui::{
     Color32, NumExt, Rect, Response, Rgba, Sense, Stroke, TextStyle, Ui, Vec2, Widget, WidgetText,
 };
 
+///
+/// A progress bar that supports both determinate (`[0->100]`) and indeterminate modes.
 #[derive(Debug, Default, Clone)]
 pub struct ProgressBar {
-    progress: f32,
-    text: String,
-    animate: bool,
-    is_indeterminate: bool,
-    desired_width: Option<f32>,
-    desired_height: Option<f32>,
+    /// Progress across the bar, in the range `[0->1]`
+    pub progress: f32,
+    /// Text to left align inside of the bar
+    pub left_text: Option<String>,
+    /// Text to center align inside of the bar
+    pub center_text: Option<String>,
+    /// Text to right align inside of the bar
+    pub right_text: Option<String>,
+    /// Request an immediate repaint of the bar
+    pub animate: bool,
+    /// The indicated progress is ignored, and the bar bounces back and forth
+    pub is_indeterminate: bool,
+    /// The desired draw width of the bar
+    pub desired_width: Option<f32>,
+    /// The desired draw height of the bar
+    pub desired_height: Option<f32>,
 }
 
 impl ProgressBar {
+    ///
+    /// Creates a new progress bar with the specified determinate progress in the range `[0,1]`
     #[must_use]
     pub fn new(progress: f32) -> ProgressBar {
         ProgressBar {
@@ -25,6 +42,10 @@ impl ProgressBar {
             ..Default::default()
         }
     }
+
+    ///
+    /// Creates a new progress bar that is indeterminate.  The little bar will bounce back and
+    /// forth to indicate stuff is happening.
     #[must_use]
     pub fn indeterminate() -> ProgressBar {
         ProgressBar {
@@ -32,10 +53,46 @@ impl ProgressBar {
             ..Default::default()
         }
     }
+
+    ///
+    /// The same as [`Self::text_left`]
     #[must_use]
     pub fn text(self, text: String) -> ProgressBar {
-        ProgressBar { text, ..self }
+        ProgressBar {
+            left_text: Some(text),
+            ..self
+        }
     }
+
+    ///
+    /// Draws the provided text left aligned
+    #[must_use]
+    pub fn text_left(self, text: String) -> ProgressBar {
+        self.text(text)
+    }
+
+    ///
+    /// Draws the provided text center aligned
+    #[must_use]
+    pub fn text_center(self, text: String) -> ProgressBar {
+        ProgressBar {
+            center_text: Some(text),
+            ..self
+        }
+    }
+
+    ///
+    /// Draws the provided text right aligned
+    #[must_use]
+    pub fn text_right(self, text: String) -> ProgressBar {
+        ProgressBar {
+            right_text: Some(text),
+            ..self
+        }
+    }
+
+    ///
+    /// Sets the desired draw width of the bar
     #[must_use]
     pub fn desired_width(self, width: f32) -> ProgressBar {
         ProgressBar {
@@ -43,6 +100,9 @@ impl ProgressBar {
             ..self
         }
     }
+
+    ///
+    /// Sets the desired draw height of the bar
     #[must_use]
     pub fn desired_height(self, height: f32) -> ProgressBar {
         ProgressBar {
@@ -58,7 +118,9 @@ impl Widget for ProgressBar {
             progress,
             desired_width,
             desired_height,
-            text,
+            left_text,
+            center_text,
+            right_text,
             animate,
             is_indeterminate,
         } = self;
@@ -108,19 +170,54 @@ impl Widget for ProgressBar {
                 Stroke::NONE,
             );
 
-            let text: WidgetText = text.into();
+            if let Some(text) = left_text {
+                let text: WidgetText = text.into();
 
-            let galley = text.into_galley(ui, Some(false), f32::INFINITY, TextStyle::Button);
-            let text_pos = outer_rect.left_center() - Vec2::new(0.0, galley.size().y / 2.0)
-                + egui::vec2(ui.spacing().item_spacing.x, 0.0);
-            let text_color = visuals
-                .override_text_color
-                .unwrap_or(visuals.selection.stroke.color);
-            galley.paint_with_fallback_color(
-                &ui.painter().with_clip_rect(outer_rect),
-                text_pos,
-                text_color,
-            );
+                let galley = text.into_galley(ui, Some(false), f32::INFINITY, TextStyle::Button);
+                let text_pos = outer_rect.left_center() - Vec2::new(0.0, galley.size().y / 2.0)
+                    + egui::vec2(ui.spacing().item_spacing.x, 0.0);
+                let text_color = visuals
+                    .override_text_color
+                    .unwrap_or(visuals.selection.stroke.color);
+                galley.paint_with_fallback_color(
+                    &ui.painter().with_clip_rect(outer_rect),
+                    text_pos,
+                    text_color,
+                );
+            }
+            if let Some(text) = center_text {
+                let text: WidgetText = text.into();
+
+                let galley = text.into_galley(ui, Some(false), f32::INFINITY, TextStyle::Button);
+                let size = galley.size();
+                let text_pos = outer_rect.center() - Vec2::new(size.x / 2.0, size.y / 2.0);
+                let text_color = visuals
+                    .override_text_color
+                    .unwrap_or(visuals.selection.stroke.color);
+                galley.paint_with_fallback_color(
+                    &ui.painter().with_clip_rect(outer_rect),
+                    text_pos,
+                    text_color,
+                );
+            }
+
+            if let Some(text) = right_text {
+                let text: WidgetText = text.into();
+
+                let galley = text.into_galley(ui, Some(false), f32::INFINITY, TextStyle::Button);
+                let size = galley.size();
+                let text_pos = outer_rect.right_center()
+                    - Vec2::new(size.x, size.y / 2.0)
+                    - egui::vec2(ui.spacing().item_spacing.x, 0.0);
+                let text_color = visuals
+                    .override_text_color
+                    .unwrap_or(visuals.selection.stroke.color);
+                galley.paint_with_fallback_color(
+                    &ui.painter().with_clip_rect(outer_rect),
+                    text_pos,
+                    text_color,
+                );
+            }
         }
 
         response
