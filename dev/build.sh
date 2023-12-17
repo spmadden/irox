c6952390c940897b8c44135e6c24766806736ebb
Sean Madden <sean@seanmadden.net>
2023-12-17 15:02:24 -0500

    feat(devimage): tweaking dev image build

--- dev/build.sh
+++ dev/build.sh
@@ -2,12 +2,15 @@
 
 set -euxo pipefail
 
+VSN=`git describe --dirty`
+TAGS="-t ghcr.io/spmadden/irox-dev:latest -t ghcr.io/spmadden/irox-dev:$VSN"
+
 if type "podman" > /dev/null; then
   echo "Found podman"
-  podman build -f Dockerfile --squash -t irox-dev:latest .
+  podman build -f Dockerfile --squash $TAGS .
 elif type "docker" > /dev/null; then
   echo "Found docker"
-  docker build -f Dockerfile -t irox-dev:latest .
+  docker build -f Dockerfile $TAGS .
 else
   echo "Could not find podman nor docker on the path"
   exit 1
--- dev/prep-environment.sh
+++ dev/prep-environment.sh
@@ -5,7 +5,6 @@ set -eux
 rustup update
 rustup component add rustfmt clippy
 TMPDIR=$(mktemp -d)
-cargo install --target-dir=$TMPDIR cargo-binstall
-cargo binstall -y cargo-about cargo-deny cargo-smart-release cargo-edit just cargo-cache
+cargo install --target-dir=$TMPDIR cargo-about cargo-deny cargo-edit just cargo-cache cargo-tarpaulin
 cargo cache -e -k0 -rall
 rm -rf $TMPDIR
