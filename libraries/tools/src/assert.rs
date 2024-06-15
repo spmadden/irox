f8a4651a0e1eb2a8c8e61431eb58493169d92ca8
Sean P. Madden <sean@seanmadden.net>
2024-06-14 21:55:16 -0400

    feat(tools): new assert::assert_eq_hex_slice method

--- libraries/tools/src/assert.rs
+++ libraries/tools/src/assert.rs
@@ -37,3 +37,27 @@ macro_rules! assert_eq_hex {
         }
     };
 }
+
+#[macro_export]
+macro_rules! assert_eq_hex_slice {
+    ($left:expr, $right:expr) => {
+        match (&$left, &$right) {
+            (left_val, right_val) => {
+                let left_len = left_val.len();
+                let right_len = right_val.len();
+                if left_len != right_len {
+                    panic!("Assertion failed, left len (0x{left_len:0X}) != right len (0x{right_len:0X})");
+                }
+                for idx in 0..left_len {
+                    let lv = left_val[idx];
+                    let rv = right_val[idx];
+                    if lv != rv {
+                        panic!(
+                            "Assertion failed, 0x{lv:0X} != 0x{rv:0X} at idx {idx}"
+                        )
+                    }
+                }
+            }
+        }
+    };
+}
