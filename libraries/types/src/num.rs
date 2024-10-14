1a87db6fda93f333ff81c62080ef1c9859bc4dbf
Sean Madden <sean@seanmadden.net>
2024-10-14 13:27:31 -0400

    fix(types): cleaning up the NumberTraits to remove a bunch of lifetimes dicking with intended uses.
    Signed-off-by: Sean Madden <sean@seanmadden.net>

--- libraries/types/src/num.rs
+++ libraries/types/src/num.rs
@@ -6,7 +6,7 @@
 //! Number Traits
 
 /// A number, signed or unsigned, floating point or integer, any size.
-pub trait Number<'a, T: Sized + 'a>:
+pub trait Number:
     core::marker::Sized
     + core::marker::Copy
     + core::marker::Send
@@ -16,23 +16,19 @@ pub trait Number<'a, T: Sized + 'a>:
     + core::fmt::Debug
     + core::fmt::LowerExp
     + core::fmt::UpperExp
-    + core::ops::Add<Output = T>
-    + core::ops::Add<&'a T, Output = T>
-    + core::ops::Sub<Output = T>
-    + core::ops::Sub<&'a T, Output = T>
-    + core::ops::Mul<Output = T>
-    + core::ops::Mul<&'a T, Output = T>
-    + core::ops::Div<Output = T>
-    + core::ops::Div<&'a T, Output = T>
-    + core::ops::Rem<Output = T>
+    + core::ops::Add
+    + core::ops::Sub
+    + core::ops::Mul
+    + core::ops::Div
+    + core::ops::Rem
     + core::str::FromStr
     + core::cmp::PartialEq
     + core::cmp::PartialOrd
 {
 }
 /// A mutable number
-pub trait NumberMut<'a, T: Sized + 'a>:
-    Number<'a, T>
+pub trait NumberMut:
+    Number
     + core::ops::AddAssign
     + core::ops::SubAssign
     + core::ops::MulAssign
@@ -42,25 +38,25 @@ pub trait NumberMut<'a, T: Sized + 'a>:
 }
 
 /// A number that can have bitwise-operations performed on it (IE, not floating point)
-pub trait NumberBits<'a, T: Sized + 'a>:
-    Number<'a, T>
+pub trait NumberBits:
+    Number
     + core::fmt::Binary
     + core::fmt::LowerHex
     + core::fmt::UpperHex
     + core::fmt::Octal
-    + core::ops::BitAnd<Output = T>
-    + core::ops::BitOr<Output = T>
-    + core::ops::BitXor<Output = T>
-    + core::ops::Not<Output = T>
-    + core::ops::Shl<Output = T>
-    + core::ops::Shr<Output = T>
+    + core::ops::BitAnd
+    + core::ops::BitOr
+    + core::ops::BitXor
+    + core::ops::Not
+    + core::ops::Shl
+    + core::ops::Shr
     + core::hash::Hash
 {
 }
 // A mutable bitwise-number (not floating point)
-pub trait NumberBitsMut<'a, T: Sized + 'a>:
-    NumberMut<'a, T>
-    + NumberBits<'a, T>
+pub trait NumberBitsMut:
+    NumberMut
+    + NumberBits
     + core::fmt::Binary
     + core::fmt::LowerHex
     + core::fmt::UpperHex
@@ -73,59 +69,68 @@ pub trait NumberBitsMut<'a, T: Sized + 'a>:
 {
 }
 /// An exact number (not floating point)
-pub trait NumberExact<'a, T: Sized + 'a>:
-    Number<'a, T> + core::cmp::Eq + core::cmp::Ord + core::hash::Hash
-{
-}
+pub trait NumberExact: Number + core::cmp::Eq + core::cmp::Ord + core::hash::Hash {}
 /// A floating point number
-pub trait NumberFloating<'a, T: Sized + 'a>:
-    Number<'a, T> + core::fmt::LowerExp + core::fmt::UpperExp
-{
-}
+pub trait NumberFloating: Number + core::fmt::LowerExp + core::fmt::UpperExp {}
 
 /// A signed number
-pub trait NumberSigned<'a, T: Sized + 'a>: Number<'a, T> + core::ops::Neg<Output = T> {}
+pub trait NumberSigned: Number + core::ops::Neg {}
 /// Any unsigned integer
-pub trait AnyUnsignedInteger<'a, T: Sized + 'a>:
-    Number<'a, T> + NumberMut<'a, T> + NumberBits<'a, T> + NumberBitsMut<'a, T> + NumberExact<'a, T>
+pub trait AnyUnsignedInteger:
+    Number + NumberMut + NumberBits + NumberBitsMut + NumberExact
 {
+    fn as_u8(&self) -> u8;
+    fn as_u16(&self) -> u16;
+    fn as_u32(&self) -> u32;
+    fn as_u64(&self) -> u64;
+    fn as_u128(&self) -> u128;
 }
-pub trait AnySignedInteger<'a, T: Sized + 'a>:
-    AnyUnsignedInteger<'a, T> + NumberSigned<'a, T>
-{
-}
-pub trait AnyFloat<'a, T: Sized + 'a>:
-    Number<'a, T> + NumberMut<'a, T> + NumberFloating<'a, T> + NumberSigned<'a, T>
-{
-}
+pub trait AnySignedInteger: AnyUnsignedInteger + NumberSigned {}
+pub trait AnyFloat: Number + NumberMut + NumberFloating + NumberSigned {}
 macro_rules! impl_exact {
     ($($typ:ty) *) => {
         $(
-            impl<'a> Number<'a, $typ> for $typ {}
-            impl<'a> NumberMut<'a, $typ> for $typ {}
-            impl<'a> NumberBits<'a, $typ> for $typ {}
-            impl<'a> NumberBitsMut<'a, $typ> for $typ {}
-            impl<'a> NumberExact<'a, $typ> for $typ {}
-            impl<'a> AnyUnsignedInteger<'a, $typ> for $typ {}
+            impl<'a> Number for $typ {}
+            impl<'a> NumberMut for $typ {}
+            impl<'a> NumberBits for $typ {}
+            impl<'a> NumberBitsMut for $typ {}
+            impl<'a> NumberExact for $typ {}
+            impl<'a> AnyUnsignedInteger for $typ {
+                fn as_u8(&self) -> u8 {
+                    *self as u8
+                }
+                fn as_u16(&self) -> u16 {
+                    *self as u16
+                }
+                fn as_u32(&self) -> u32 {
+                    *self as u32
+                }
+                fn as_u64(&self) -> u64 {
+                    *self as u64
+                }
+                fn as_u128(&self) -> u128 {
+                    *self as u128
+                }
+            }
         )*
     };
 }
 macro_rules! impl_float {
     ($($typ:ty) *) => {
         $(
-            impl<'a> Number<'a, $typ> for $typ {}
-            impl<'a> NumberMut<'a, $typ> for $typ {}
-            impl<'a> NumberSigned<'a, $typ> for $typ {}
-            impl<'a> NumberFloating<'a, $typ> for $typ {}
-            impl<'a> AnyFloat<'a, $typ> for $typ {}
+            impl<'a> Number for $typ {}
+            impl<'a> NumberMut for $typ {}
+            impl<'a> NumberSigned for $typ {}
+            impl<'a> NumberFloating for $typ {}
+            impl<'a> AnyFloat for $typ {}
         )*
     };
 }
 macro_rules! impl_signed {
     ($($typ:ty) *) => {
         $(
-            impl<'a> NumberSigned<'a, $typ> for $typ {}
-            impl<'a> AnySignedInteger<'a, $typ> for $typ {}
+            impl<'a> NumberSigned for $typ {}
+            impl<'a> AnySignedInteger for $typ {}
         )*
     };
 }
