bd167018b58f553bd980c076257bf3540bdde145
Sean Madden <sean@seanmadden.net>
2024-10-24 22:18:27 -0400

    fix(metrics): fixed metrics bench

--- libraries/metrics/benches/metrics_bench.rs
+++ libraries/metrics/benches/metrics_bench.rs
@@ -2,16 +2,17 @@
 // Copyright 2024 IROX Contributors
 //
 
-use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
-use irox_metrics::{random_data, RoadWarrior};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use irox_metrics::{random_data, random_keys, RoadWarrior};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
+    let (pubk, _privk) = random_keys();
     let data = random_data();
-    let mut eph = RoadWarrior::new();
+    let mut eph = RoadWarrior::new(pubk);
 
     c.bench_with_input(BenchmarkId::new("encode", ""), &data, |b, data| {
         b.iter(|| {
-            eph.seal(&data);
+            eph.seal(&data).unwrap();
         });
     });
 }
