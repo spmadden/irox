76eaf78eda488c5a0a9207bad1807351cf0bc7e7
Sean Madden <sean@seanmadden.net>
2025-01-07 22:52:59 -0500

    feat(plots): use new errorbars and repainter

--- libraries/egui_extras/examples/plotsperf.rs
+++ libraries/egui_extras/examples/plotsperf.rs
@@ -6,9 +6,10 @@ use eframe::{App, CreationContext, Frame, HardwareAcceleration, Renderer};
 use egui::{CentralPanel, Context, ThemePreference, Ui, Vec2, ViewportBuilder};
 use irox_egui_extras::logplot::{
     x_axis_time_millis_formatter, y_axis_units_formatter, Axis, AxisAlignmentMode, BasicPlot,
-    LineWithErrorBars, PlotPoint, YAxisSide,
+    ErrorBarsType, LineWithErrorBars, PlotPoint, YAxisSide,
 };
-use irox_egui_extras::toolframe::ToolApp;
+use irox_egui_extras::repainting::{RepaintManager, RepaintRequest};
+use irox_egui_extras::toolframe::{ToolApp, ToolFrame, ToolFrameOptions};
 use irox_stats::windows::{
     BinStatistics, SavitszkyGolaySmoother24Builder, TimeWindow, TimedLinearSlopeFilter,
     TimedWindowFilter, WindowBinStrategy,
@@ -23,6 +24,7 @@ use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use std::thread::JoinHandle;
 
+#[allow(clippy::print_stderr)]
 pub fn main() {
     let viewport = ViewportBuilder::default().with_inner_size(Vec2::new(1024., 800.));
     #[cfg(feature = "profiling")]
@@ -43,7 +45,7 @@ pub fn main() {
         renderer: Renderer::Glow,
         hardware_acceleration: HardwareAcceleration::Preferred,
         multisampling: 2,
-        vsync: true,
+        vsync: false,
         ..Default::default()
     };
 
@@ -52,26 +54,25 @@ pub fn main() {
         native_options,
         Box::new(|cc| {
             cc.egui_ctx.set_theme(DEFAULT_THEME);
-            Ok(
-                // Box::new(ToolFrame::new_opts(
-                // cc,
-                Box::new(TestApp::new(cc)), // ToolFrameOptions {
-                                            //     show_rendering_stats: true,
-                                            //     ..Default::default()
-                                            // },
-                                            // )
-            )
+            Ok(Box::new(ToolFrame::new_opts(
+                cc,
+                Box::new(TestApp::new(cc)),
+                ToolFrameOptions {
+                    show_rendering_stats: true,
+                    ..Default::default()
+                },
+            )))
         }),
     ) {
         error!("{e:?}");
     };
 }
-const NUM_LINES_PER_PLOT: usize = 16;
+const NUM_LINES_PER_PLOT: usize = 1;
 const NUM_PLOTS: usize = 1;
 const DATA_RATE: Duration = Duration::from_millis(10); // 100 hz data
 const MAX_DATA_TO_KEEP: Duration = Duration::from_minutes(5);
 const AVERAGING_WINDOW: Duration = Duration::from_seconds_f64(0.5);
-const MAX_REPAINT_RATE: Duration = Duration::from_seconds_f64(1. / 20.); // 60hz
+const MAX_REPAINT_RATE: Duration = Duration::from_seconds_f64(1. / 5.); // 10hz
 const LINE_CTR: f64 = 5e-6;
 const LINE_JITTER: f64 = 1e-6;
 const LINE_INCR: f64 = 1e-8;
@@ -99,27 +100,45 @@ impl Drop for PlotOpts {
 pub struct TestApp {
     plots: Vec<PlotOpts>,
     running: Arc<AtomicBool>,
+    repainter: Option<RepaintManager>,
 }
 impl TestApp {
     pub fn new(cc: &CreationContext) -> Self {
         let mut plots = Vec::new();
         let running = Arc::new(AtomicBool::new(true));
         cc.egui_ctx.memory_mut(|_mem| {
-            let t = &mut _mem.options.tessellation_options;
-            t.feathering = false;
+            // let t = &mut _mem.options.tessellation_options;
+            // t.feathering = false;
             // t.feathering_size_in_pixels = 0.0;
         });
         let mut num_lines = NUM_LINES_PER_PLOT;
+        let repainter = RepaintManager::new(MAX_REPAINT_RATE, cc.egui_ctx.clone());
+
         for _pidx in 0..NUM_PLOTS {
-            plots.push(Self::spawn_thread(running.clone(), &cc.egui_ctx, num_lines));
+            let repainter = repainter.requester();
+            plots.push(Self::spawn_thread(
+                running.clone(),
+                &repainter,
+                &cc.egui_ctx,
+                num_lines,
+            ));
             num_lines /= 2;
             num_lines *= 2;
         }
 
-        Self { plots, running }
+        Self {
+            plots,
+            running,
+            repainter: Some(repainter),
+        }
     }
 
-    fn spawn_thread(running: Arc<AtomicBool>, ctx: &Context, num_lines: usize) -> PlotOpts {
+    fn spawn_thread(
+        running: Arc<AtomicBool>,
+        repainter: &RepaintRequest,
+        ctx: &Context,
+        num_lines: usize,
+    ) -> PlotOpts {
         let mut data_plot = BasicPlot::new(ctx)
             .with_x_axis_formatter(x_axis_time_millis_formatter())
             .with_y_axis_formatter(y_axis_units_formatter(Units::Volt));
@@ -140,23 +159,24 @@ impl TestApp {
             let (_, error_bars) = data_plot.add_line_with_error_bars(|line| {
                 line.set_name(format!("Line {}", lidx + 1));
             });
-            let troc_line = if TROC_OVERLAY {
+            let (_, troc_line) = if TROC_OVERLAY {
                 data_plot.y_axis_right = Some(Axis {
                     // alignment_mode: AxisAlignmentMode::CenterOnZeroWithOppsositeRange,
                     alignment_mode: AxisAlignmentMode::CenterOnZero,
                     axis_formatter: Some(y_axis_units_formatter(Units::Volt)),
                     ..Default::default()
                 });
-                data_plot.add_line(|line| {
+                data_plot.add_line_with_error_bars(|line| {
                     line.set_name(format!("Line {} TROC", lidx + 1));
                     line.yaxis_side = YAxisSide::RightAxis;
                 })
             } else {
-                troc_plot.add_line(|line| {
+                // troc_plot.y_axis_left.alignment_mode = AxisAlignmentMode::CenterOnZero;
+                troc_plot.add_line_with_error_bars(|line| {
                     line.set_name(format!("Line {} TROC", lidx + 1));
                 })
             };
-            let ctx = ctx.clone();
+            let repainter = repainter.clone();
             let hndl = std::thread::spawn(move || {
                 let mut last_run = UnixTimestamp::now();
                 let mut average_filter = TimedWindowFilter::new(
@@ -168,8 +188,9 @@ impl TestApp {
                 let mut error_data = BinStatistics::new(AVERAGING_WINDOW);
 
                 let mut troc_filter =
-                    TimedLinearSlopeFilter::new(AVERAGING_WINDOW * 5., WindowBinStrategy::Center);
-                let mut troc_data = TimeWindow::new(MAX_DATA_TO_KEEP - AVERAGING_WINDOW * 5.);
+                    TimedLinearSlopeFilter::new(AVERAGING_WINDOW, WindowBinStrategy::Center);
+                let mut troc_data = TimeWindow::new(MAX_DATA_TO_KEEP - AVERAGING_WINDOW);
+                let mut troc_stddevs = BinStatistics::new(AVERAGING_WINDOW * 20.);
 
                 let mut rnd = irox_tools::random::PcgXshRR::default();
 
@@ -184,6 +205,7 @@ impl TestApp {
                         }
                         if DO_TROC {
                             if let Some(value) = troc_filter.insert(curiter, val) {
+                                troc_stddevs.insert(value.time, value.value);
                                 troc_data.add_sample(value);
                             }
                         }
@@ -209,7 +231,6 @@ impl TestApp {
                 }
 
                 let start_time = now;
-                let mut last_repaint = now;
                 while running.load(Ordering::Relaxed) {
                     let diff = DATA_RATE - last_run.elapsed();
                     std::thread::sleep(diff.into());
@@ -226,11 +247,13 @@ impl TestApp {
                     }
                     if DO_TROC {
                         if let Some(data) = troc_filter.insert(last_run, new_val) {
+                            troc_stddevs.insert(data.time, data.value);
                             troc_data.add_sample(data);
                         }
                     }
 
                     error_data.remove_data_before(olders);
+
                     let eb_data = error_data
                         .iter()
                         .map(|(_idx, bin)| (bin.start.get_offset().as_millis() as f64, bin.summary))
@@ -242,18 +265,28 @@ impl TestApp {
                     error_bars.replace_data(LineWithErrorBars {
                         line_data: Arc::from(line_data),
                         error_bars: Arc::from(eb_data),
+                        // error_bars_type: ErrorBarsType::StdDev,
+                        ..Default::default()
                     });
 
                     if DO_TROC {
+                        troc_stddevs.remove_data_before(olders);
+                        let tr_stddev_data = troc_stddevs
+                            .iter()
+                            .map(|(_idx, bin)| {
+                                (bin.start.get_offset().as_millis() as f64, bin.summary)
+                            })
+                            .collect::<Vec<_>>();
                         let tr_pts = troc_data.map_data(|(t, v)| {
                             PlotPoint::new(t.get_offset().as_millis() as f64, *v)
                         });
-                        troc_line.replace_data(Arc::from(tr_pts));
+                        troc_line.replace_data(LineWithErrorBars {
+                            line_data: Arc::from(tr_pts),
+                            error_bars: Arc::from(tr_stddev_data),
+                            error_bars_type: ErrorBarsType::StdDev,
+                        });
                     }
-                    if (last_run - last_repaint) > MAX_REPAINT_RATE {
-                        last_repaint = last_run;
-                        ctx.request_repaint();
-                    }
+                    repainter.request();
                 }
             });
             handles.push(hndl);
@@ -278,24 +311,26 @@ impl TestApp {
 impl Drop for TestApp {
     fn drop(&mut self) {
         self.running.store(false, Ordering::Relaxed);
+        if let Some(rp) = self.repainter.take() {
+            drop(rp);
+        }
     }
 }
 
 impl App for TestApp {
     fn update(&mut self, ctx: &Context, _frame: &mut Frame) {
         CentralPanel::default().show(ctx, |ui| {
-            let mut num_plots = 0;
-            for plot in &self.plots {
-                num_plots += plot.plots.len();
-            }
+            let num_plots = self.plots.len();
             let rem = ui.available_size();
-            let each = Vec2::new(rem.x, rem.y / num_plots as f32);
+            let each = Vec2::new(rem.x / 2., rem.y / num_plots as f32);
             for plot in &mut self.plots {
-                for plot in &mut plot.plots {
-                    ui.allocate_ui(each, |ui| {
-                        plot.show(ui);
-                    });
-                }
+                ui.horizontal(|ui| {
+                    for plot in &mut plot.plots {
+                        ui.allocate_ui(each, |ui| {
+                            plot.show(ui);
+                        });
+                    }
+                });
             }
         });
     }
