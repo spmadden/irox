d0c38822ffb13c9a7fb300a402161a43e339816c
Sean Madden <sean@seanmadden.net>
2025-01-12 20:41:39 -0500

    feat(tools): tests for rotating matrices

--- libraries/tools/src/math/matrix.rs
+++ libraries/tools/src/math/matrix.rs
@@ -4,13 +4,16 @@
 
 #![allow(clippy::indexing_slicing)]
 
-use crate::ToSigned;
-use core::iter::Sum;
-use core::ops::{Add, Deref, DerefMut, Index, IndexMut, Mul, Sub};
+use crate::{ToF64, ToSigned};
+use core::ops::{Add, AddAssign, Deref, DerefMut, Index, IndexMut, Mul, Sub};
+
+pub trait AsMatrix<const M: usize, const N: usize, T: Sized + Copy + Default> {
+    fn as_matrix(&self) -> Matrix<M, N, T>;
+}
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub struct Matrix<const M: usize, const N: usize, T: Sized + Copy + Default> {
-    values: [[T; N]; M],
+    pub values: [[T; N]; M],
 }
 
 impl<const M: usize, const N: usize, T: Sized + Copy + Default> Matrix<M, N, T> {
@@ -19,24 +22,43 @@ impl<const M: usize, const N: usize, T: Sized + Copy + Default> Matrix<M, N, T> 
         Matrix { values }
     }
 }
+impl<const M: usize, const N: usize, T: Sized + Copy + Default> From<[[T; N]; M]>
+    for Matrix<M, N, T>
+{
+    fn from(value: [[T; N]; M]) -> Self {
+        Self { values: value }
+    }
+}
+impl<const M: usize, const N: usize, T: Sized + Copy + Default> From<&[[T; N]; M]>
+    for Matrix<M, N, T>
+{
+    fn from(value: &[[T; N]; M]) -> Self {
+        Self { values: *value }
+    }
+}
+impl<const M: usize, const N: usize, T: Sized + Copy + Default> AsMatrix<M, N, T> for [[T; N]; M] {
+    fn as_matrix(&self) -> Matrix<M, N, T> {
+        self.into()
+    }
+}
 impl<const M: usize, const N: usize> Matrix<M, N, f64> {
     #[must_use]
-    pub const fn mul<const P: usize>(&self, other: Matrix<P, M, f64>) -> Matrix<P, N, f64> {
-        let mut out = [[0.0; N]; P];
-        let mut i = 0;
-        while i < M {
-            let mut j = 0;
-            while j < P {
-                let mut k = 0;
+    pub const fn mul<const P: usize>(&self, other: Matrix<N, P, f64>) -> Matrix<M, P, f64> {
+        let mut out = [[0.0f64; P]; M];
+        let mut m = 0;
+        while m < M {
+            let mut p = 0;
+            while p < P {
+                let mut n = 0;
                 let mut sum = 0.0;
-                while k < N {
-                    sum += self.values[i][k] * other.values[k][j];
-                    k += 1;
+                while n < N {
+                    sum += self.values[m][n] * other.values[n][p];
+                    n += 1;
                 }
-                out[i][j] = sum;
-                j += 1;
+                out[m][p] = sum;
+                p += 1;
             }
-            i += 1;
+            m += 1;
         }
         Matrix { values: out }
     }
@@ -166,14 +188,35 @@ impl Matrix<2, 2, f64> {
 impl Matrix<3, 1, f64> {
     #[must_use]
     pub const fn translate(&self, x: f64, y: f64) -> Self {
-        Matrix::mul(self, Matrix::new([[1., 0., x], [0., 1., y], [0., 0., 1.]]))
+        Matrix::mul(
+            &Matrix::new([[1., 0., x], [0., 1., y], [0., 0., 1.]]),
+            *self,
+        )
+    }
+    cfg_feature_std! {
+        #[must_use]
+        pub fn rotate_x<T: ToF64 + Copy>(&self, angle: T) -> Self {
+            Matrix::<3, 3, f64>::rotated_x(angle).mul(*self)
+        }
+        #[must_use]
+        pub fn rotate_y<T: ToF64 + Copy>(&self, angle: T) -> Self {
+            Matrix::<3, 3, f64>::rotated_y(angle).mul(*self)
+        }
+        #[must_use]
+        pub fn rotate_z<T: ToF64 + Copy>(&self, angle: T) -> Self {
+            Matrix::<3, 3, f64>::rotated_z(angle).mul(*self)
+        }
+        #[must_use]
+        pub fn rotate_zyx<T: ToF64 + Copy>(&self, x_angle: T, y_angle: T, z_angle: T) -> Self {
+            Matrix::<3, 3, f64>::rotated_zyx(x_angle, y_angle, z_angle).mul(*self)
+        }
     }
 }
-
 impl Matrix<3, 3, f64> {
     cfg_feature_std! {
         #[must_use]
-        pub fn rotate_x(angle: f64) -> Self {
+        pub fn rotated_x<T: ToF64 + Copy>(angle: T) -> Self {
+        let angle = angle.to_f64();
             Self::new([
                 [1., 0., 0.],
                 [0., angle.cos(), -angle.sin()],
@@ -181,20 +224,43 @@ impl Matrix<3, 3, f64> {
             ])
         }
         #[must_use]
-        pub fn rotate_y(angle: f64) -> Self {
-            Self::new([
-                [angle.cos(), 0.0, angle.sin()],
-                [0., 1., 0.],
-                [-angle.sin(), 0., angle.cos()],
-            ])
+        pub fn rotate_x<T: ToF64 + Copy>(&self, angle: T) -> Self {
+            self.mul(Self::rotated_x(angle))
         }
         #[must_use]
-        pub fn rotate_z(angle: f64) -> Self {
-            Self::new([
-                [angle.cos(), -angle.sin(), 0.],
-                [angle.sin(), angle.cos(), 0.],
-                [0., 0., 1.],
-            ])
+        pub fn rotated_y<T: ToF64 + Copy>(angle: T) -> Self {
+            let angle = angle.to_f64();
+                Self::new([
+                    [angle.cos(), 0.0, angle.sin()],
+                    [0., 1., 0.],
+                    [-angle.sin(), 0., angle.cos()],
+                ])
+            }
+            #[must_use]
+        pub fn rotate_y<T: ToF64 + Copy>(&self, angle: T) -> Self {
+            self.mul(Self::rotated_y(angle))
+        }
+        #[must_use]
+        pub fn rotated_z<T: ToF64 + Copy>(angle: T) -> Self {
+            let angle = angle.to_f64();
+                Self::new([
+                    [angle.cos(), angle.sin(), 0.],
+                    [-angle.sin(), angle.cos(), 0.],
+                    [0., 0., 1.],
+                ])
+            }
+        #[must_use]
+        pub fn rotate_z<T: ToF64 + Copy>(&self, angle: T) -> Self {
+            self.mul(Self::rotated_z(angle))
+        }
+
+        #[must_use]
+        pub fn rotated_zyx<T: ToF64 + Copy>(x_angle: T, y_angle: T, z_angle: T) -> Self {
+            Self::rotated_z(z_angle).rotate_y(y_angle).rotate_x(x_angle)
+        }
+        #[must_use]
+        pub fn rotate_zyx<T: ToF64 + Copy>(&self, x_angle: T, y_angle: T, z_angle: T) -> Self {
+            self.rotate_z(z_angle).rotate_y(y_angle).rotate_x(x_angle)
         }
     }
 }
@@ -226,26 +292,44 @@ impl<const M: usize, const N: usize, T: Sized + Copy + Default> DerefMut for Mat
         &mut self.values
     }
 }
-impl<
-        const M: usize,
-        const N: usize,
-        const P: usize,
-        T: Sized + Copy + Default + Add + Mul + Sum<<T as Mul<T>>::Output>,
-    > Mul<Matrix<N, P, T>> for Matrix<M, N, T>
-{
-    type Output = Matrix<M, P, T>;
-    fn mul(self, other: Matrix<N, P, T>) -> Matrix<M, P, T> {
-        let mut out = [[T::default(); P]; M];
-        for (i, ith) in out.iter_mut().enumerate().take(M) {
-            for (j, val) in ith.iter_mut().enumerate().take(P) {
-                *val = (0..N)
-                    .map(|k| self.values[i][k] * other.values[k][j])
-                    .sum::<T>();
+// matrix multiply
+macro_rules! impl_mul {
+    ($($ty:ty)+) => {
+        impl<
+            const M: usize,
+            const N: usize,
+            const P: usize,
+            T: Sized + Copy + Default + Add + Mul + AddAssign<<T as Mul<T>>::Output>,
+            > Mul<Matrix<N, P, T>> for $($ty)+
+        {
+            type Output = Matrix<M, P, T>;
+            fn mul(self, other: Matrix<N, P, T>) -> Matrix<M, P, T> {
+                let mut out = [[T::default(); P]; M];
+                let mut m = 0;
+                while m < M {
+                    let mut p = 0;
+                    while p < P {
+                        let mut n = 0;
+                        let mut sum = T::default();
+                        while n < N {
+                            sum += self.values[m][n] * other.values[n][p];
+                            n += 1;
+                        }
+                        out[m][p] = sum;
+                        p += 1;
+                    }
+                    m += 1;
+                }
+                Matrix { values: out }
             }
         }
-        Matrix { values: out }
-    }
+    };
 }
+impl_mul!(Matrix<M, N, T>);
+impl_mul!(&Matrix<M, N, T>);
+impl_mul!(&mut Matrix<M, N, T>);
+
+// matrix add
 impl<const M: usize, const N: usize, T: Sized + Copy + Default + Add<Output = T>> Add
     for Matrix<M, N, T>
 {
@@ -261,6 +345,7 @@ impl<const M: usize, const N: usize, T: Sized + Copy + Default + Add<Output = T>
         Matrix { values: out }
     }
 }
+// scalar multiply
 impl<const M: usize, const N: usize, T: Sized + Copy + Default + Mul<T, Output = T>> Mul<T>
     for Matrix<M, N, T>
 {
@@ -293,7 +378,8 @@ impl<
 
 #[cfg(test)]
 mod test {
-    use crate::math::Matrix;
+    use crate::math::{AsMatrix, Matrix};
+    use core::ops::Deref;
 
     #[test]
     pub fn test_scalar() {
@@ -308,5 +394,35 @@ mod test {
         let m2 = Matrix::new([[7, 8], [9, 10], [11, 12]]);
         let res = m1 * m2;
         assert_eq!(res, Matrix::new([[58, 64], [139, 154]]));
+    }
+
+    #[cfg(feature = "std")]
+    #[test]
+    pub fn test_rotate1() {
+        let m = [[3.], [7.], [4.]].as_matrix();
+        let [[x], [y], [z]] = m.rotate_x(core::f64::consts::FRAC_PI_2).deref().clone();
+        assert_eq_eps!(3., x, 2. * f64::EPSILON);
+        assert_eq_eps!(-4., y, 2. * f64::EPSILON);
+        assert_eq_eps!(7., z, 2. * f64::EPSILON);
+    }
+
+    #[cfg(feature = "std")]
+    #[test]
+    pub fn test_rotate2() {
+        let m = [[3.], [7.], [4.]].as_matrix();
+        let [[x], [y], [z]] = m.rotate_y(core::f64::consts::FRAC_PI_2).deref().clone();
+        assert_eq_eps!(4., x, 2. * f64::EPSILON);
+        assert_eq_eps!(7., y, 2. * f64::EPSILON);
+        assert_eq_eps!(-3., z, 2. * f64::EPSILON);
+    }
+
+    #[cfg(feature = "std")]
+    #[test]
+    pub fn test_rotate3() {
+        let m = [[3.], [7.], [4.]].as_matrix();
+        let [[x], [y], [z]] = m.rotate_z(core::f64::consts::FRAC_PI_2).deref().clone();
+        assert_eq_eps!(7., x, 2. * f64::EPSILON);
+        assert_eq_eps!(-3., y, 2. * f64::EPSILON);
+        assert_eq_eps!(4., z, 2. * f64::EPSILON);
     }
 }
