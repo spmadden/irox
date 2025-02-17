ed10f3e02cf7bb2e432c25067be2cee58a57778e
Sean Madden <sean@seanmadden.net>
2025-02-16 20:44:16 -0500

    feat(tools): Add `array!` macro for array construction
    This new macro simplifies the creation of arrays with repeated elements. It supports various sizes using recursive accumulation logic, improving code clarity and reducing repetitive boilerplate.
    
    Signed-off-by: Sean Madden <sean@seanmadden.net>

--- libraries/tools/src/arrays.rs
+++ libraries/tools/src/arrays.rs
@@ -1,10 +1,42 @@
 // SPDX-License-Identifier: MIT
-// Copyright ${YEAR} IROX Contributors
+// Copyright 2025 IROX Contributors
 //
 
 //!
 //! Functions and tools for manipulating arrays of items.
 //!
+
+#[macro_export]
+macro_rules! array {
+    (@accum (0, $($_es:expr),*) -> ($($body:tt)*))
+        => {array!(@as_expr [$($body)*])};
+    (@accum (1, $($es:expr),*) -> ($($body:tt)*))
+        => {array!(@accum (0, $($es),*) -> ($($body)* $($es,)*))};
+    (@accum (2, $($es:expr),*) -> ($($body:tt)*))
+        => {array!(@accum (0, $($es),*) -> ($($body)* $($es,)* $($es,)*))};
+    (@accum (3, $($es:expr),*) -> ($($body:tt)*))
+        => {array!(@accum (2, $($es),*) -> ($($body)* $($es,)*))};
+    (@accum (4, $($es:expr),*) -> ($($body:tt)*))
+        => {array!(@accum (2, $($es,)* $($es),*) -> ($($body)*))};
+    (@accum (5, $($es:expr),*) -> ($($body:tt)*))
+        => {array!(@accum (4, $($es),*) -> ($($body)* $($es,)*))};
+    (@accum (6, $($es:expr),*) -> ($($body:tt)*))
+        => {array!(@accum (4, $($es),*) -> ($($body)* $($es,)* $($es,)*))};
+    (@accum (7, $($es:expr),*) -> ($($body:tt)*))
+        => {array!(@accum (4, $($es),*) -> ($($body)* $($es,)* $($es,)* $($es,)*))};
+    (@accum (8, $($es:expr),*) -> ($($body:tt)*))
+        => {array!(@accum (4, $($es,)* $($es),*) -> ($($body)*))};
+    (@accum (16, $($es:expr),*) -> ($($body:tt)*))
+        => {array!(@accum (8, $($es,)* $($es),*) -> ($($body)*))};
+    (@accum (32, $($es:expr),*) -> ($($body:tt)*))
+        => {array!(@accum (16, $($es,)* $($es),*) -> ($($body)*))};
+    (@accum (64, $($es:expr),*) -> ($($body:tt)*))
+        => {array!(@accum (32, $($es,)* $($es),*) -> ($($body)*))};
+
+    (@as_expr $e:expr) => {$e};
+
+    [$e:expr; $n:tt] => { array!(@accum ($n, $e) -> ()) };
+}
 
 ///
 /// Scans through the array, looking for the maximum value contained within it, using the
