2794f0f8209f681621fb462accc30ec8ae65ecc9
Sean Madden <sean@seanmadden.net>
2025-03-01 10:39:25 -0500

    feat(tools): Add Murmur3_32 implementation and hashing benchmarks
    This commit introduces the Murmur3_32 struct and its associated functionality. It also refactors parts of the Murmur3_128 implementation and integrates both into benchmarking tests. Additionally, new benchmarks were added to evaluate Murmur3 and SHA hashing performance.

---
+++ libraries/tools/benches/hashes.rs
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: MIT
+// Copyright 2025 IROX Contributors
+//
+
+use criterion::{criterion_group, criterion_main, Criterion, Throughput};
+use irox_tools::hash::murmur3::{Murmur3_128, Murmur3_32};
+use irox_tools::hash::{SHA256, SHA512};
+use std::time::Duration;
+
+struct Hasher {
+    iter: [u8; 128],
+}
+impl Default for Hasher {
+    fn default() -> Self {
+        Self { iter: [0; 128] }
+    }
+}
+impl Hasher {
+    pub fn hash_murmur_3(&mut self) {
+        let hash = Murmur3_128::new();
+        let _ = hash.hash(&self.iter);
+        self.iter[0] += 1;
+    }
+    pub fn hash_murmur_32(&mut self) {
+        let hash = Murmur3_32::new();
+        let _ = hash.hash(&self.iter);
+        self.iter[0] += 1;
+    }
+    pub fn hash_sha356(&mut self) {
+        let _hash = SHA256::new().hash(&self.iter);
+        self.iter[0] += 1;
+    }
+    pub fn hash_sha512(&mut self) {
+        let _hash = SHA512::new().hash(&self.iter);
+        self.iter[0] += 1;
+    }
+}
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    let mut hasher = Hasher::default();
+    let mut grp = c.benchmark_group("sha512");
+    grp.throughput(Throughput::Bytes(128));
+    grp.bench_function("hash_sha512", |b| {
+        b.iter(|| {
+            hasher.hash_sha512();
+        })
+    });
+    grp.finish();
+    std::thread::sleep(Duration::from_secs(20));
+    let mut grp = c.benchmark_group("sha256");
+    grp.throughput(Throughput::Bytes(128));
+    grp.bench_function("hash_sha256", |b| {
+        b.iter(|| {
+            hasher.hash_sha356();
+        })
+    });
+    grp.finish();
+    std::thread::sleep(Duration::from_secs(20));
+    let mut grp = c.benchmark_group("murmur3_128");
+    grp.throughput(Throughput::Bytes(128));
+    grp.bench_function("hash_murmur_3", |b| {
+        b.iter(|| {
+            hasher.hash_murmur_3();
+        })
+    });
+    grp.finish();
+    std::thread::sleep(Duration::from_secs(20));
+    let mut grp = c.benchmark_group("murmur3_32");
+    grp.throughput(Throughput::Bytes(128));
+    grp.bench_function("hash_murmur_32", |b| {
+        b.iter(|| {
+            hasher.hash_murmur_32();
+        })
+    });
+    grp.finish();
+}
+criterion_group!(hashes, criterion_benchmark);
+criterion_main!(hashes);
--- libraries/tools/src/hash/murmur3.rs
+++ libraries/tools/src/hash/murmur3.rs
@@ -6,28 +6,185 @@
 //! Implementation of Murmurhash3.  Currently only x128 implemented.
 //!
 
+use crate::buf::{Buffer, FixedU8Buf};
 use core::ops::BitXorAssign;
-use irox_bits::Bits;
+use irox_bits::{array_split_8, Bits};
 
 const C1: u64 = 0x87c3_7b91_1142_53d5;
 const C2: u64 = 0x4cf5_ad43_2745_937f;
 const C3: u64 = 0xff51_afd7_ed55_8ccd;
 const C4: u64 = 0xc4ce_b9fe_1a85_ec53;
 
-///
-/// Runs the murmur3_128 hash function with no seed
-pub fn murmur3_128<T: AsRef<[u8]>>(key: T) -> u128 {
-    murmur3_128_seed(key, 0)
-}
+const C5: u32 = 0xCC9E2D51;
+const C6: u32 = 0x1B873593;
 
 macro_rules! fmix64 {
-    ($k:ident) => {
+    ($k:expr) => {
         $k.bitxor_assign($k >> 33);
         $k = $k.wrapping_mul(C3);
         $k.bitxor_assign($k >> 33);
         $k = $k.wrapping_mul(C4);
         $k.bitxor_assign($k >> 33);
     };
+}
+#[derive(Default)]
+pub struct Murmur3_32 {
+    h: u32,
+    buf: FixedU8Buf<4>,
+    total_len: u64,
+}
+impl Murmur3_32 {
+    pub fn new() -> Self {
+        Default::default()
+    }
+    pub fn new_seeded(seed: u32) -> Self {
+        Self {
+            h: seed,
+            total_len: 0,
+            buf: Default::default(),
+        }
+    }
+    pub fn write<T: AsRef<[u8]>>(&mut self, key: T) {
+        for v in key.as_ref() {
+            let _ = self.buf.push(*v);
+            self.total_len += 1;
+            self.try_chomp();
+        }
+    }
+    fn try_chomp(&mut self) {
+        if !self.buf.is_full() {
+            return;
+        }
+        let k = self.buf.as_buf_default();
+        let k = u32::from_le_bytes(k);
+        self.h ^= k.wrapping_mul(C5).rotate_left(15).wrapping_mul(C6);
+        self.h = self.h.rotate_left(13);
+        self.h = self.h.wrapping_mul(5).wrapping_add(0xe6546b64);
+    }
+    pub fn hash<T: AsRef<[u8]>>(mut self, key: T) -> u32 {
+        self.write(key);
+        self.finish()
+    }
+    pub fn finish(mut self) -> u32 {
+        let k = if !self.buf.is_empty() {
+            let k = self.buf.as_buf_default();
+            u32::from_le_bytes(k)
+        } else {
+            0
+        };
+        self.h ^= k.wrapping_mul(C5).rotate_left(15).wrapping_mul(C6);
+        self.h ^= self.total_len as u32;
+        self.h ^= self.h >> 16;
+        self.h = self.h.wrapping_mul(0x85ebca6b);
+        self.h ^= self.h >> 13;
+        self.h = self.h.wrapping_mul(0xc2b2ae35);
+        self.h ^= self.h >> 16;
+        self.h
+    }
+}
+
+#[derive(Default)]
+pub struct Murmur3_128 {
+    h1: u64,
+    h2: u64,
+    buf: FixedU8Buf<16>,
+    total_len: u64,
+}
+impl Murmur3_128 {
+    pub fn new() -> Self {
+        Default::default()
+    }
+    pub fn new_seeded(seed: u64) -> Self {
+        Self {
+            h1: seed,
+            h2: seed,
+            total_len: 0,
+            buf: Default::default(),
+        }
+    }
+    pub fn hash<T: AsRef<[u8]>>(mut self, key: T) -> u128 {
+        self.write(key);
+        self.finish()
+    }
+    pub fn write<T: AsRef<[u8]>>(&mut self, key: T) {
+        for v in key.as_ref() {
+            let _ = self.buf.push(*v);
+            self.total_len += 1;
+            self.try_chomp();
+        }
+    }
+    fn try_chomp(&mut self) {
+        if !self.buf.is_full() {
+            return;
+        }
+        let chunk_16 = self.buf.as_buf_default();
+        let (k1, k2) = array_split_8(chunk_16);
+        let k1 = u64::from_le_bytes(k1);
+        let k2 = u64::from_le_bytes(k2);
+        self.h1
+            .bitxor_assign(k1.wrapping_mul(C1).rotate_left(31).wrapping_mul(C2));
+        self.h1 = self
+            .h1
+            .rotate_left(27)
+            .wrapping_add(self.h2)
+            .wrapping_mul(5)
+            .wrapping_add(0x52dce729);
+        self.h2
+            .bitxor_assign(k2.wrapping_mul(C2).rotate_left(33).wrapping_mul(C1));
+        self.h2 = self
+            .h2
+            .rotate_left(31)
+            .wrapping_add(self.h1)
+            .wrapping_mul(5)
+            .wrapping_add(0x38495ab5);
+    }
+    pub fn finish(mut self) -> u128 {
+        if !self.buf.is_empty() {
+            let mut k1: u64 = 0;
+            let mut k2: u64 = 0;
+            let len = self.buf.len();
+            let mut iter = self.buf.iter();
+
+            let mut shift: u32 = 0;
+            for i in 0..len {
+                let Some(val) = iter.next() else {
+                    break;
+                };
+                let val: u64 = *val as u64;
+                if i == 8 {
+                    shift = 0;
+                }
+                if i >= 8 {
+                    k2.bitxor_assign(val.wrapping_shl(shift));
+                } else {
+                    k1.bitxor_assign(val.wrapping_shl(shift));
+                }
+                shift += 8;
+            }
+            if len > 8 {
+                self.h2
+                    .bitxor_assign(k2.wrapping_mul(C2).rotate_left(33).wrapping_mul(C1));
+            }
+            self.h1
+                .bitxor_assign(k1.wrapping_mul(C1).rotate_left(31).wrapping_mul(C2));
+        }
+        self.h1.bitxor_assign(self.total_len);
+        self.h2.bitxor_assign(self.total_len);
+        self.h1 = self.h1.wrapping_add(self.h2);
+        self.h2 = self.h2.wrapping_add(self.h1);
+        fmix64!(self.h1);
+        fmix64!(self.h2);
+        self.h1 = self.h1.wrapping_add(self.h2);
+        self.h2 = self.h2.wrapping_add(self.h1);
+
+        ((self.h1 as u128) << 64) | self.h2 as u128
+    }
+}
+
+///
+/// Runs the murmur3_128 hash function with no seed
+pub fn murmur3_128<T: AsRef<[u8]>>(key: T) -> u128 {
+    murmur3_128_seed(key, 0)
 }
 
 ///
@@ -108,6 +265,7 @@ mod test {
     use crate::hash::murmur3_128;
     use alloc::vec;
     extern crate alloc;
+    use crate::hash::murmur3::{Murmur3_128, Murmur3_32};
     use alloc::vec::Vec;
 
     #[test]
@@ -159,6 +317,39 @@ mod test {
         for (data, exp) in tests {
             let hash = murmur3_128(data.as_bytes());
             assert_eq!(exp, hash);
+            let hash = Murmur3_128::new().hash(data.as_bytes());
+            assert_eq!(exp, hash);
+            let mut hash = Murmur3_128::new();
+            hash.write(data.as_bytes());
+            let hash = hash.finish();
+            assert_eq!(exp, hash);
+        }
+    }
+
+    #[test]
+    pub fn test_32() {
+        let tests = [
+            ("", 0x00000000u32, 0x00000000u32),
+            ("", 0x00000001, 0x514E28B7),
+            ("", 0xFFFFFFFF, 0x81F16F39),
+            ("test", 0x00000000, 0xBA6BD213),
+            ("test", 0x9747B28C, 0x704B81DC),
+            ("Hello, world!", 0x00000000, 0xC0363E43),
+            ("Hello, world!", 0x9747B28C, 0x24884CBA),
+            (
+                "The quick brown fox jumps over the lazy dog",
+                0x00000000,
+                0x2E4FF723,
+            ),
+            (
+                "The quick brown fox jumps over the lazy dog",
+                0x9747B28C,
+                0x2FA826CD,
+            ),
+        ];
+        for (inp, seed, exp) in tests {
+            let hash = Murmur3_32::new_seeded(seed).hash(inp.as_bytes());
+            assert_eq_hex!(exp, hash);
         }
     }
 }
