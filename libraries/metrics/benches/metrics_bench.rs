c3096d1c0909e27d660593b3500348e3d117806c
Sean P. Madden <sean@seanmadden.net>
2025-10-15 18:03:47 -0400

    fix(metrics): fix annoying build warning in metrics

--- libraries/metrics/benches/metrics_bench.rs
+++ libraries/metrics/benches/metrics_bench.rs
@@ -3,19 +3,18 @@
 //
 
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
-use irox_metrics::{random_data, random_keys, Error, RoadWarrior};
+use irox_metrics::{random_data, random_keys, RoadWarrior};
 
-pub fn criterion_benchmark(c: &mut Criterion) -> Result<(), Error> {
-    let (pubk, _privk) = random_keys()?;
-    let data = random_data()?;
-    let mut eph = RoadWarrior::new(pubk)?;
+pub fn criterion_benchmark(c: &mut Criterion) {
+    let (pubk, _privk) = random_keys().unwrap();
+    let data = random_data().unwrap();
+    let mut eph = RoadWarrior::new(pubk).unwrap();
 
     c.bench_with_input(BenchmarkId::new("encode", ""), &data, |b, data| {
         b.iter(|| {
             eph.seal(data).unwrap();
         });
     });
-    Ok(())
 }
 
 criterion_group!(benches, criterion_benchmark);
