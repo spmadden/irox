13e25487cc41d3ba530be94fa054d59ff1553398
Sean P. Madden <sean@seanmadden.net>
2026-02-01 23:24:24 -0500

    fix(tools): readding unsafe forbidden flag

--- libraries/tools/src/lib.rs
+++ libraries/tools/src/lib.rs
@@ -2,7 +2,7 @@
 // Copyright 2025 IROX Contributors
 //
 
-// #![forbid(unsafe_code)]
+#![forbid(unsafe_code)]
 #![warn(clippy::alloc_instead_of_core)]
 #![warn(clippy::std_instead_of_alloc)]
 #![warn(clippy::std_instead_of_core)]
