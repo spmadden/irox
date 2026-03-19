e1a91d505bb5f69615d934df12afda5ab1f5b8dc
Sean Madden <sean@seanmadden.net>
2026-03-18 21:14:06 -0400

    feat(tools): impl of BCR-2020-012 Bytewords

---
+++ libraries/tools/src/hash/bytewords.rs
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: MIT
+// Copyright 2025 IROX Contributors
+//
+
+//! BCR-2020-012 Bytewords
+
+use core::fmt::Write;
+
+pub static WORDLIST: [&str; 256] = [
+    "able", "acid", "also", "apex", "aqua", "arch", "atom", "aunt", "away", "axis", "back", "bald",
+    "barn", "belt", "beta", "bias", "blue", "body", "brag", "brew", "bulb", "buzz", "calm", "cash",
+    "cats", "chef", "city", "claw", "code", "cola", "cook", "cost", "crux", "curl", "cusp", "cyan",
+    "dark", "data", "days", "deli", "dice", "diet", "door", "down", "draw", "drop", "drum", "dull",
+    "duty", "each", "easy", "echo", "edge", "epic", "even", "exam", "exit", "eyes", "fact", "fair",
+    "fern", "figs", "film", "fish", "fizz", "flap", "flew", "flux", "foxy", "free", "frog", "fuel",
+    "fund", "gala", "game", "gear", "gems", "gift", "girl", "glow", "good", "gray", "grim", "guru",
+    "gush", "gyro", "half", "hang", "hard", "hawk", "heat", "help", "high", "hill", "holy", "hope",
+    "horn", "huts", "iced", "idea", "idle", "inch", "inky", "into", "iris", "iron", "item", "jade",
+    "jazz", "join", "jolt", "jowl", "judo", "jugs", "jump", "junk", "jury", "keep", "keno", "kept",
+    "keys", "kick", "kiln", "king", "kite", "kiwi", "knob", "lamb", "lava", "lazy", "leaf", "legs",
+    "liar", "limp", "lion", "list", "logo", "loud", "love", "luau", "luck", "lung", "main", "many",
+    "math", "maze", "memo", "menu", "meow", "mild", "mint", "miss", "monk", "nail", "navy", "need",
+    "news", "next", "noon", "note", "numb", "obey", "oboe", "omit", "onyx", "open", "oval", "owls",
+    "paid", "part", "peck", "play", "plus", "poem", "pool", "pose", "puff", "puma", "purr", "quad",
+    "quiz", "race", "ramp", "real", "redo", "rich", "road", "rock", "roof", "ruby", "ruin", "runs",
+    "rust", "safe", "saga", "scar", "sets", "silk", "skew", "slot", "soap", "solo", "song", "stub",
+    "surf", "swan", "taco", "task", "taxi", "tent", "tied", "time", "tiny", "toil", "tomb", "toys",
+    "trip", "tuna", "twin", "ugly", "undo", "unit", "urge", "user", "vast", "very", "veto", "vial",
+    "vibe", "view", "visa", "void", "vows", "wall", "wand", "warm", "wasp", "wave", "waxy", "webs",
+    "what", "when", "whiz", "wolf", "work", "yank", "yawn", "yell", "yoga", "yurt", "zaps", "zero",
+    "zest", "zinc", "zone", "zoom",
+];
+
+pub fn get_word(data: u8) -> &'static str {
+    if let Some(s) = WORDLIST.get(data as usize) {
+        s
+    } else {
+        ""
+    }
+}
+pub fn iter_words<F: FnMut(&str)>(data: &[u8], mut f: F) {
+    for d in data {
+        f(get_word(*d));
+    }
+}
+pub fn get_words<const N: usize>(data: [u8; N]) -> [&'static str; N] {
+    let mut out = [""; N];
+    for (d, out) in data.iter().zip(&mut out) {
+        *out = get_word(*d);
+    }
+    out
+}
+pub fn write_words<T: Write>(data: &[u8], sep: &str, out: &mut T) -> core::fmt::Result {
+    let mut first = true;
+    for d in data {
+        let word = get_word(*d);
+        if !first {
+            write!(out, "{sep}")?;
+        }
+        first = false;
+        write!(out, "{word}")?;
+    }
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use crate::hash::bytewords::{get_word, get_words, WORDLIST};
+
+    #[test]
+    pub fn test1() {
+        for d in 0u8..=255 {
+            let word = get_word(d);
+            let exp = WORDLIST.get(d as usize).copied();
+            assert_eq!(exp, Some(word));
+        }
+    }
+    #[test]
+    pub fn test2() {
+        assert_eq!(["able"], get_words([0x0]));
+        assert_eq!(["judo", "chef"], get_words([0x70, 0x19]));
+    }
+}
--- libraries/tools/src/hash/mod.rs
+++ libraries/tools/src/hash/mod.rs
@@ -21,6 +21,7 @@ pub use sha1::SHA1;
 pub use sha2::{SHA224, SHA256, SHA384, SHA512};
 
 mod blake2;
+pub mod bytewords;
 pub mod md5;
 pub mod murmur3;
 pub mod sha1;
