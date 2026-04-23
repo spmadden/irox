573407d1b5f54f6dd8e15fdd77758109f6136495
Sean Madden <sean@seanmadden.net>
2026-04-23 10:08:34 -0400

    feat(graphing): easier graph filtering & inversion
    Signed-off-by: Sean Madden <sean@seanmadden.net>

--- libraries/graphing/src/graph.rs
+++ libraries/graphing/src/graph.rs
@@ -4,7 +4,7 @@
 
 #![allow(clippy::redundant_closure_for_method_calls)]
 
-use crate::{Edge, SharedEdge, SharedEdgeIdentifier, SharedNode, SharedNodeIdentifier};
+use crate::{Edge, Node, SharedEdge, SharedEdgeIdentifier, SharedNode, SharedNodeIdentifier};
 use core::ops::Deref;
 use std::collections::HashMap;
 
@@ -15,6 +15,9 @@ pub struct Graph {
 }
 
 impl Graph {
+    pub fn new() -> Self {
+        Self::default()
+    }
     pub fn add_node(&mut self, node: SharedNode) -> Result<(), String> {
         let Some(d) = node.descriptor(|v| v.cloned()) else {
             return Err(format!("Can't retrieve descriptor from {node:?}"));
@@ -27,7 +30,8 @@ impl Graph {
         let Some(d) = edge.descriptor() else {
             return Err(format!("Can't retrieve descriptor from {edge:?}"));
         };
-        self.edges.insert(d.id.clone().into(), edge.clone());
+        let id: SharedEdgeIdentifier = d.id.clone().into();
+        self.edges.insert(id.clone(), edge.clone());
         let Ok(lock) = edge.inner.read() else {
             return Err(format!("Can't get edge info for {d:?}"));
         };
@@ -35,18 +39,73 @@ impl Graph {
             Edge::Directed { from, to, .. } => {
                 self.nodes
                     .get(from)
-                    .and_then(|v| v.add_navigable_edge(&edge).err());
-                self.nodes.get(to).and_then(|v| v.add_edge(&edge).err());
+                    .and_then(|v| v.add_navigable_edge(&id).err());
+                self.nodes.get(to).and_then(|v| v.add_edge(&id).err());
             }
             Edge::Undirected { left, right, .. } => {
                 self.nodes
                     .get(left)
-                    .and_then(|v| v.add_navigable_edge(&edge).err());
+                    .and_then(|v| v.add_navigable_edge(&id).err());
                 self.nodes
                     .get(right)
-                    .and_then(|v| v.add_navigable_edge(&edge).err());
+                    .and_then(|v| v.add_navigable_edge(&id).err());
             }
         }
         Ok(())
+    }
+    pub fn remove_edge(&mut self, id: &SharedEdgeIdentifier) {
+        self.edges.remove(id);
+    }
+
+    pub fn remove_node(&mut self, id: &SharedNodeIdentifier) {
+        if let Some(node) = self.nodes.remove(id) {
+            if let Ok(lock) = node.read_lock() {
+                for edge in &lock.all_edges {
+                    self.remove_edge(edge);
+                }
+            }
+        }
+    }
+
+    ///
+    /// Creates a graph with all navigable edges inverted
+    #[must_use]
+    pub fn inverted_edges(&self) -> Self {
+        let mut outgraph = Graph::new();
+        for node in self.nodes.values() {
+            node.get(|node| {
+                let _ = outgraph.add_node(
+                    Node {
+                        descriptor: node.descriptor.clone(),
+                        navigable_edges: vec![],
+                        all_edges: vec![],
+                    }
+                    .into(),
+                );
+            });
+        }
+        for edge in self.edges.values() {
+            if edge.is_directed() {
+                edge.get(|f| {
+                    if let Edge::Directed {
+                        descriptor,
+                        from,
+                        to,
+                    } = f
+                    {
+                        let newedge: SharedEdge = Edge::Directed {
+                            descriptor: descriptor.clone(),
+                            from: to.clone(),
+                            to: from.clone(),
+                        }
+                        .into();
+                        let _ = outgraph.add_edge(newedge);
+                    }
+                });
+            } else {
+                let _ = outgraph.add_edge(edge.clone());
+            }
+        }
+        outgraph
     }
 }
