c3cba0d89ab75dd21e29a79c212e7a8f18edaf9c
Sean Madden <sean@seanmadden.net>
2026-08-02 15:50:04 -0400

    fix(sqlite3): fix tests
    Signed-off-by: Sean Madden <sean@seanmadden.net>

--- interfaces/sqlite3/tests/header.rs
+++ interfaces/sqlite3/tests/header.rs
@@ -1,11 +1,14 @@
+// SPDX-License-Identifier: MIT
+// Copyright 2026 IROX Contributors
+//
+
 use irox_sqlite3::db::Database;
 
 #[test]
 fn read_header() {
     // let mut file = File::open().expect("open");
 
-    let mut db =
-        Database::open_db_path(&"C:\\chartdata\\NOAA MBTiles\\ncds_03.mbtiles").expect("Ugh.");
+    let mut db = Database::open_db_path(&"./tests/test.db").expect("Ugh.");
     println!("{:#?}", db);
 
     let page = db.read_page(0).expect("Ugh");
